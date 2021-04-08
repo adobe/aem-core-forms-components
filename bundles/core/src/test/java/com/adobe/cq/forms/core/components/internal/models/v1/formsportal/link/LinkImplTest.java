ac13386edcf042951476327de42248d303f55914
Viresh Gupta <virresh@users.noreply.github.com>
2021-04-08 16:26:21 +0530

    CQ-4301939 Add JUnit tests
    And hide Asset Type selector since IC is not yet available
    
    @review @rismehta @sakshi-arora1

--- bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/formsportal/link/LinkImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/formsportal/link/LinkImplTest.java
@@ -15,4 +15,73 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.forms.core.components.internal.models.v1.formsportal.link;
 
-public class LinkImplTest {}
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.adobe.cq.forms.core.components.models.formsportal.Link;
+import com.adobe.cq.forms.core.context.FormsCoreComponentTestContext;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+@ExtendWith(AemContextExtension.class)
+public class LinkImplTest {
+
+    public final AemContext context = FormsCoreComponentTestContext.newAemContext();
+
+    private static final String TEST_BASE = "/link";
+    private static final String CONTENT_ROOT = "/content";
+    private static final String ROOT_PAGE = CONTENT_ROOT + "/fplink";
+    private static final String EMPTY_LINK_PATH = ROOT_PAGE + "/linkcomponent-empty";
+    private static final String LINK1_PATH = ROOT_PAGE + "/linkcomponent-v1";
+    private static final String LINK1_PATH_WITH_INVALID_LINK = ROOT_PAGE + "/linkcomponent-v1-invalidref";
+
+    @BeforeEach
+    public void setUp() {
+        context.load().json(TEST_BASE + FormsCoreComponentTestContext.TEST_CONTENT_JSON, CONTENT_ROOT);
+    }
+
+    @Test
+    public void testEmptyLinkComponent() {
+        Link link = getLinkUnderTest(EMPTY_LINK_PATH);
+        Assertions.assertEquals(null, link.getAssetPath());
+        Assertions.assertEquals("#", link.getRenderUrl());
+        Assertions.assertEquals("_self", link.getLinkTarget());
+        Assertions.assertEquals(null, link.getLinkText());
+        Assertions.assertEquals(null, link.getLinkTooltip());
+        Assertions.assertEquals(null, link.getAssetType());
+        Assertions.assertEquals(null, link.getQueryParams());
+    }
+
+    @Test
+    public void testLinkComponent() {
+        Link link = getLinkUnderTest(LINK1_PATH);
+        Assertions.assertEquals("/content/dam/formsanddocuments/sample-form", link.getAssetPath());
+        Assertions.assertEquals("/content/dam/formsanddocuments/sample-form/jcr:content?wcmmode=disabled", link.getRenderUrl());
+        Assertions.assertEquals("_blank", link.getLinkTarget());
+        Assertions.assertEquals("Link Component", link.getLinkText());
+        Assertions.assertEquals("Some Hover Tooltip Text", link.getLinkTooltip());
+        Assertions.assertEquals(Link.AssetType.ADAPTIVE_FORM, link.getAssetType());
+        Assertions.assertEquals(1, link.getQueryParams().size());
+    }
+
+    @Test
+    public void testLinkComponentWithInvalidPath() {
+        Link link = getLinkUnderTest(LINK1_PATH_WITH_INVALID_LINK);
+        Assertions.assertEquals("https://www.adobe.com/", link.getAssetPath());
+        Assertions.assertEquals("https://www.adobe.com/?hello", link.getRenderUrl());
+        Assertions.assertEquals("_blank", link.getLinkTarget());
+        Assertions.assertEquals("Link Component", link.getLinkText());
+        Assertions.assertEquals("Some Hover Tooltip Text", link.getLinkTooltip());
+        Assertions.assertEquals(Link.AssetType.ADAPTIVE_FORM, link.getAssetType());
+        Assertions.assertEquals(1, link.getQueryParams().size());
+    }
+
+    private Link getLinkUnderTest(String resourcePath) {
+        MockSlingHttpServletRequest mockRequest = context.request();
+        mockRequest.setResource(context.currentResource(resourcePath));
+        return mockRequest.adaptTo(Link.class);
+    }
+}
