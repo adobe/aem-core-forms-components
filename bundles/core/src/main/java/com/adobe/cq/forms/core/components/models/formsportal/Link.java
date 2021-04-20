34e0ee63da3ccdb1a1bf9d259b180f29d9c6cde8
Viresh Gupta <virresh@users.noreply.github.com>
2021-04-20 10:34:10 +0530

    CQ-4301939 Add Link Interface javadocs
    @review @rismehta @vdua

--- bundles/core/src/main/java/com/adobe/cq/forms/core/components/models/formsportal/Link.java
+++ bundles/core/src/main/java/com/adobe/cq/forms/core/components/models/formsportal/Link.java
@@ -20,33 +20,81 @@ import org.osgi.annotation.versioning.ConsumerType;
 import com.adobe.cq.wcm.core.components.models.Component;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * Defines the sling model for {@code /apps/core/fd/components/formsportal/link} component.
+ *
+ * @since com.adobe.cq.forms.core.components.models.aemform 1.0.0
+ */
 @ConsumerType
 public interface Link extends Component {
+
+    /**
+     * Defines the Asset Type that is being linked to
+     */
     enum AssetType {
         ADAPTIVE_FORM
     }
 
+    /**
+     * Returns url to which the component is linking to
+     * The URL contains query parameters if configured
+     *
+     * @return URL after processing, or {@code #} if none is set
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     default String getUrl() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns the text shown in Link
+     *
+     * @return Title string. It is a mandatory field.
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     default String getTitle() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Return the text shown on hovering over link
+     *
+     * @return Tooltip string. Can return null if not set
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     default String getTooltip() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Return the strategy with which linked url should be opened.
+     * Refer to {@code <a>} html tag attribute {@code target} for semantics
+     *
+     * @return Target strategy string
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     @JsonIgnore
     default String getTarget() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Return the asset type that is linked to
+     *
+     * @return Asset Type if set
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     default AssetType getAssetType() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns path to the asset to which the URL would redirect to
+     * Note that this is not the url. For url, use {@link #getUrl}
+     *
+     * @return Path to asset if configured
+     * @since com.adobe.cq.forms.core.components.models.formsportal 1.0.0
+     */
     default String getAssetPath() {
         throw new UnsupportedOperationException();
     }
