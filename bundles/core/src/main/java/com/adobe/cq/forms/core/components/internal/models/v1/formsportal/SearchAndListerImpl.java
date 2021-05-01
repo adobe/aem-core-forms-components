0490ce566f0e88830b077ff52bfecfda9629127b
Viresh Gupta <virresh@users.noreply.github.com>
2021-05-02 00:04:19 +0530

    CQ-4301935 Add Search and Lister Component
    - Initial Component code
    - JUnit tests for Sling Models
    - Support for multiple search and lister elements on a page
    - Basic Sanity check with cypress
    - Initial PortalLister Interface
    - Delegation pattern for re-using PortalLister interface
    - Client side rendering now takes data from sling model json
    - Seperate template clientlibs
    
    @review @sakshi-arora1 @rismehta @vdua

---
+++ bundles/core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/formsportal/SearchAndListerImpl.java
@@ -0,0 +1,119 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2021 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+package com.adobe.cq.forms.core.components.internal.models.v1.formsportal;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.forms.core.components.internal.models.v1.AbstractComponentImpl;
+import com.adobe.cq.forms.core.components.models.formsportal.PortalLister;
+import com.adobe.cq.forms.core.components.models.formsportal.SearchAndLister;
+
+@Model(
+    adaptables = SlingHttpServletRequest.class,
+    adapters = { SearchAndLister.class, ComponentExporter.class },
+    resourceType = SearchAndListerImpl.RESOURCE_TYPE,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(
+    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class SearchAndListerImpl extends AbstractComponentImpl implements SearchAndLister {
+    public static final String RESOURCE_TYPE = "core/fd/components/formsportal/searchnlister/v1/searchnlister";
+    private static final String DEFAULT_TITLE = "Forms Portal";
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Inject
+    private String title;
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Inject
+    private String layout;
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(booleanValues = false)
+    @Inject
+    private boolean disableSearch;
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(booleanValues = false)
+    @Inject
+    private boolean disableTextSearch;
+
+    @Self
+    @Via(type = ResourceSuperType.class)
+    private PortalLister commonLister;
+
+    @Override
+    public String getTitle() {
+        if (StringUtils.isEmpty(title)) {
+            // using isEmpty instead of isBlank to allow only whitespace in title
+            return DEFAULT_TITLE;
+        }
+        return title;
+    }
+
+    @Override
+    public String getLayout() {
+        if (StringUtils.isEmpty(layout)) {
+            return "Card";
+        }
+        return layout;
+    }
+
+    @Override
+    public boolean getAdvancedSearchDisabled() {
+        return disableSearch;
+    }
+
+    @Override
+    public boolean getTextSearchDisabled() {
+        return disableTextSearch;
+    }
+
+    @Override
+    public long getResultLimit() {
+        return Long.parseLong(commonLister.getLimit());
+    }
+
+    @Override
+    public Map<String, Object> getSearchResults() {
+        List<PortalLister.Item> results = commonLister.getResultList();
+        Long offset = commonLister.getNextOffset();
+        Map<String, Object> jacksonMapping = new HashMap<>();
+        jacksonMapping.put("data", results);
+        jacksonMapping.put("success", "true");
+        jacksonMapping.put("nextOffset", offset);
+        return jacksonMapping;
+    }
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/core/fd/components/commons/v1/clientlibs/authoringvalidators/js/multifield-min-max.js
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright 2021 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+(function ($) {
+    "use strict";
+
+    $.validator.register("foundation.validation.validator", {
+        selector: "coral-multifield",
+        validate: function(el) {
+
+            var totalPanels = el["0"].items.getAll().length;
+            var min;
+            var max;
+            if ($(el).data("min-item")){
+                min = $(el).data("min-item");
+                if(totalPanels < min) {
+                    return "Minimum numbers of items required are: " + min;
+                }
+            }
+            if ($(el).data("max-item")){
+                max = $(el).data("max-item");
+                if(totalPanels > max) {
+                    return "Maximum numbers of items allowed are: " + max;
+                }
+            }
+        }
+    });
+})(jQuery);
