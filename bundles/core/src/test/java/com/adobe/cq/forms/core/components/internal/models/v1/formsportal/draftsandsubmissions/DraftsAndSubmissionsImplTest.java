d8890f0e7dc9e8d5d90417cadf5322cb68da297c
Viresh Gupta <virresh@users.noreply.github.com>
2021-12-22 12:27:09 +0530

    Add tests for more diff coverage

--- bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/formsportal/draftsandsubmissions/DraftsAndSubmissionsImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/formsportal/draftsandsubmissions/DraftsAndSubmissionsImplTest.java
@@ -18,15 +18,19 @@ package com.adobe.cq.forms.core.components.internal.models.v1.formsportal.drafts
 
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.List;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import com.adobe.aem.formsndocuments.assets.models.AdaptiveFormAsset;
 import com.adobe.cq.forms.core.Utils;
@@ -41,6 +45,10 @@ import com.adobe.fd.fp.api.models.DraftModel;
 import com.adobe.fd.fp.api.models.SubmitModel;
 import com.adobe.fd.fp.api.service.DraftService;
 import com.adobe.fd.fp.api.service.SubmitService;
+import com.adobe.forms.foundation.usc.model.Query;
+import com.adobe.forms.foundation.usc.model.Statement;
+import com.adobe.forms.foundation.usc.model.Statement.Operator;
+import com.adobe.forms.foundation.usc.model.StatementGroup;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
@@ -51,6 +59,7 @@ public class DraftsAndSubmissionsImplTest {
     private static final String ROOT_PAGE = CONTENT_ROOT + "/fpdns";
     private static final String DRAFT_COMPONENT_PATH = ROOT_PAGE + "/dns-draft-v1";
     private static final String SUBMISSION_COMPONENT_PATH = ROOT_PAGE + "/dns-submission-v1";
+    private static final String DEFAULT_COMPONENT_PATH = ROOT_PAGE + "/dns-default-v1";
     private static final String SAMPLE_FORM = CONTENT_ROOT + "/dam/formsanddocuments/sample-form";
     public final AemContext context = FormsCoreComponentTestContext.newAemContext();
 
@@ -125,6 +134,48 @@ public class DraftsAndSubmissionsImplTest {
         Assertions.assertThrows(UnsupportedOperationException.class, component::getType);
     }
 
+    @Test
+    public void testDefaultTitle() {
+        DraftsAndSubmissions component = getInstanceUnderTest(DEFAULT_COMPONENT_PATH);
+        Assertions.assertEquals("Drafts", component.getTitle());
+    }
+
+    @Test
+    public void testException() throws FormsPortalException {
+        Resource afDamRes = context.resourceResolver().getResource(SAMPLE_FORM);
+        AdaptiveFormAsset mockAsset = getMockAFAssetOf(afDamRes, true);
+        context.registerAdapter(Resource.class, AdaptiveFormAsset.class, mockAsset);
+
+        Mockito.when(draftModel.getLastModifiedTime()).thenReturn(new Calendar.Builder().setInstant(12345678)
+            .build());
+        Mockito.when(draftModel.getFormPath()).thenReturn(SAMPLE_FORM);
+
+        // to assert private getters inside QueryImpl, StatementImpl and StatementGroup Impl
+        Mockito.when(draftService.getAllDraft(Mockito.any())).then(new Answer<List<DraftModel>>() {
+            @Override
+            public List<DraftModel> answer(InvocationOnMock invocation) throws Throwable {
+                Query q = (Query) invocation.getArgument(0);
+                Assert.assertEquals(8, q.getLimit());
+                Assert.assertEquals(0, q.getOffset());
+                Assert.assertNotNull(q.getStatementGroup());
+
+                StatementGroup sg = q.getStatementGroup();
+                Assert.assertNull(sg.getJoinOperator());
+                Assert.assertEquals(1, sg.getStatements().size());
+
+                Statement stmt = sg.getStatements().get(0);
+                Assert.assertNotNull(stmt);
+                Assert.assertEquals("owner", stmt.getAttributeName());
+                Assert.assertEquals("admin", stmt.getAttributeValue());
+                Assert.assertEquals(Operator.EQUALS, stmt.getOperator());
+                return Collections.singletonList(draftModel);
+            }
+        });
+
+        DraftsAndSubmissions component = getInstanceUnderTest(DEFAULT_COMPONENT_PATH);
+        Assert.assertEquals(2, component.getElements().size());
+    }
+
     private DraftsAndSubmissions getInstanceUnderTest(String resourcePath) {
         MockSlingHttpServletRequest mockRequest = context.request();
         mockRequest.setResource(context.currentResource(resourcePath));
@@ -132,9 +183,17 @@ public class DraftsAndSubmissionsImplTest {
     }
 
     private AdaptiveFormAsset getMockAFAssetOf(Resource res) {
+        return getMockAFAssetOf(res, false);
+    }
+
+    private AdaptiveFormAsset getMockAFAssetOf(Resource res, boolean invalidLink) {
         AdaptiveFormAsset mockAsset = Mockito.mock(AdaptiveFormAsset.class);
         ValueMap vm = res.getChild("jcr:content/metadata").getValueMap();
-        Mockito.when(mockAsset.getRenderLink()).thenReturn("/render/link/by/backend/api");
+        if (invalidLink) {
+            Mockito.when(mockAsset.getRenderLink()).thenReturn("/render/link/by/backend with spaces to make it invalid/api");
+        } else {
+            Mockito.when(mockAsset.getRenderLink()).thenReturn("/render/link/by/backend/api");
+        }
         Mockito.when(mockAsset.getTitle()).thenReturn(vm.get("title", "Form Title"));
         Mockito.when(mockAsset.getDescription()).thenReturn(vm.get("description", "Form Description"));
         Mockito.when(mockAsset.getThumbnailPath()).thenReturn("/thumbnail/path/by/backend");
