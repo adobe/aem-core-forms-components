b0ee49322848be556f3b1ec876cc5503a84c68ce
Rishi Mehta <rismehta@adobe.com>
2022-04-11 12:00:53 +0530

    CQ-4343453 Adding support for form container component for headless beta @review @vdua DOD(Yes)

---
+++ bundles/core/src/main/java/com/adobe/cq/forms/core/components/internal/servlets/FormMetaDataDataSourceServlet.java
@@ -0,0 +1,159 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.internal.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.jetbrains.annotations.NotNull;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.adobe.aemds.guide.model.FormMetaData;
+import com.adobe.granite.ui.components.Config;
+import com.adobe.granite.ui.components.ExpressionResolver;
+import com.adobe.granite.ui.components.ds.DataSource;
+import com.adobe.granite.ui.components.ds.SimpleDataSource;
+import com.day.cq.wcm.foundation.forms.FormsManager;
+
+@Component(
+    service = { Servlet.class },
+    property = {
+        "sling.servlet.resourceTypes=core/fd/components/formcontainer/datasource",
+        "sling.servlet.methods=GET",
+        "sling.servlet.extensions=html"
+    })
+public class FormMetaDataDataSourceServlet extends AbstractDataSourceServlet {
+
+    private static final String TYPE = "type";
+    private static final String DATA_MODEL = "guideDataModel";
+    private static final String NN_DIALOG = "cq:dialog";
+
+    /**
+     * Defines the form meta data type. Possible values: {@code submitAction}, {@code prefillServiceProvider}
+     *
+     * @todo: Add other metadata types here like fragment, actions etc
+     */
+    public enum FormMetaDataType {
+        SUBMIT_ACTION("submitAction");
+        private String value;
+
+        FormMetaDataType(String value) {
+            this.value = value;
+        }
+
+        /**
+         * Given a {@link String} <code>value</code>, this method returns the enum's value that corresponds to the provided string
+         * representation. If no representation is found,
+         *
+         * @param value the string representation for which an enum value should be returned
+         * @return the corresponding enum value, if one was found
+         */
+        public static FormMetaDataType fromString(String value) {
+            for (FormMetaDataType type : FormMetaDataType.values()) {
+                if (StringUtils.equals(value, type.value)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Returns the string value of this enum constant.
+         *
+         * @return the string value of this enum constant
+         */
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    @Reference
+    private transient ExpressionResolver expressionResolver;
+
+    @NotNull
+    @Override
+    protected ExpressionResolver getExpressionResolver() {
+        return expressionResolver;
+    }
+
+    @Override
+    protected void doGet(@NotNull SlingHttpServletRequest request, @NotNull SlingHttpServletResponse response)
+        throws ServletException, IOException {
+        Config config = getConfig(request);
+        SimpleDataSource actionTypeDataSource = null;
+        if (config != null) {
+            FormMetaDataType type = FormMetaDataType.fromString(getParameter(config, TYPE, request, null));
+            String dataModel = getParameter(config, DATA_MODEL, request, "");
+            actionTypeDataSource = new SimpleDataSource(getDataSourceResources(
+                request.getResourceResolver(), type, dataModel).iterator());
+        }
+        request.setAttribute(DataSource.class.getName(), actionTypeDataSource);
+    }
+
+    private List<Resource> getDataSourceResources(ResourceResolver resourceResolver, FormMetaDataType type, String dataModel) {
+        List<Resource> resources = new ArrayList<>();
+        FormMetaData formMetaData = resourceResolver.adaptTo(FormMetaData.class);
+        if (formMetaData != null) {
+            Iterator<FormsManager.ComponentDescription> metaDataList = null;
+            switch (type) {
+                case SUBMIT_ACTION:
+                    // filter the submit actions by uniqueness and data model
+                    Set<String> uniques = new HashSet<>();
+                    metaDataList = StreamSupport.stream(Spliterators.spliteratorUnknownSize(formMetaData.getSubmitActions(),
+                        Spliterator.ORDERED), false)
+                        .filter(e -> uniques.add(e.getResourceType())) // In case of overlay, we honor only one
+                        .filter(e -> {
+                            // only return submit action based on data model configured
+                            return resourceResolver.getResource(e.getResourceType()).getValueMap().get(DATA_MODEL, "").toLowerCase()
+                                .contains(dataModel.toLowerCase());
+                        })
+                        .collect(Collectors.toList()).iterator();
+                    break;
+            }
+
+            if (metaDataList != null) {
+                while (metaDataList.hasNext()) {
+                    FormsManager.ComponentDescription description = metaDataList.next();
+                    Resource syntheticResource = createResource(resourceResolver, description);
+                    resources.add(syntheticResource);
+                }
+            }
+        }
+        return resources;
+    }
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/core/fd/components/commons/clientlibs/editor/utils/utils.js
@@ -0,0 +1,166 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+(function($, channel, Coral) {
+    "use strict";
+
+    window.CQ = window.CQ || {};
+    window.CQ.FormsCoreComponents = window.CQ.FormsCoreComponents || {};
+    window.CQ.FormsCoreComponents.Utils = window.CQ.FormsCoreComponents.Utils || {};
+
+    /**
+     * Utility class which can be used during authoring
+     */
+    window.CQ.FormsCoreComponents.Utils.v1 = class Utils {
+        constructor() {
+            if (this instanceof Utils) {
+                throw Error('A static class cannot be instantiated.');
+            }
+        }
+        /**
+         * Toggles the display of the given elements based on the condition
+         * @param {HTMLElement} components The html elements to show/hide
+         */
+        static checkAndDisplay(components) {
+            return function(condition) {
+                var elemArray = components instanceof Array ? components : [components];
+                elemArray.forEach(function(elem) {
+                    if (condition()) {
+                        elem.show()
+                    } else {
+                        elem.hide()
+                    }
+                });
+            }
+        }
+
+        /**
+         * Manipulates the component name and value
+         * @param {HTMLElement} components The html elements to show/hide
+         */
+        static manipulateNameAndValue(components) {
+            return function(newNames, newValues) {
+                var elemArray = components instanceof Array ? components : [components];
+                elemArray.forEach(function(elem) {
+                    if (typeof newNames[index] !== 'undefined' && newNames[index] != null && elem) {
+                        elem.name = newNames[index];
+                    }
+                    if (typeof newValues[index] !== 'undefined' && newValues[index] != null && elem) {
+                        elem.value = newValues[index];
+                    }
+                });
+            }
+        }
+
+        /**
+         * encodes <script> and </script> with &lt;script&gt; and *lt;/script&gt;
+         * and also img,video and audio tag respectively
+         *
+         * other tags are being removed since scripts can be run through
+         * <img onerror="script" /> (same for audio and video)
+         */
+        static encodeScriptableTags (str) {
+            var retStr = str;
+            if (typeof str === 'string' || str instanceof String) {
+                retStr = str.replace(/<(\/?)(script[^<>]*)>/gi, '&lt;$1$2&gt;')
+                    .replace(/<(\/?)(img[^<>]*)>/gi, '&lt;$1$2&gt;')
+                    .replace(/<(\/?)(video[^<>]*)>/gi, '&lt;$1$2&gt;')
+                    .replace(/<(\/?)(audio[^<>]*)>/gi, '&lt;$1$2&gt;');
+            }
+            return retStr;
+        }
+
+        /**
+         * Get selected radio option helper
+         * @param component The radio option component
+         * @returns {String} Value of the selected radio option
+         */
+        static getSelectedRadioGroupOption(component) {
+            var radioComp = component.find('[type="radio"]');
+            for (var i = 0; i < radioComp.length; i++) {
+                if ($(radioComp[i]).prop("checked")) {
+                    return $(radioComp[i]).val();
+                }
+            }
+            return undefined;
+        }
+
+        /**
+         * Initialise the edit dialog based on its class name
+         * @param {String} editDialogClass CSS class of the edit dialog
+         */
+        static initializeEditDialog(editDialogClass) {
+            return function() {
+                var args = Array.prototype.slice.call(arguments);
+                /**
+                 * Initialise the conditional display of the various elements of the dialog.
+                 *
+                 * @param {HTMLElement} dialog The dialog on which the operation is to be performed.
+                 */
+                function initialise(dialog) {
+                    dialog = $(dialog);
+                    args.forEach(function(x){x(dialog)});
+                }
+                channel.on("foundation-contentloaded", function(e) {
+                    if ($(e.target).find(editDialogClass).length > 0) {
+                        Coral.commons.ready(e.target, function(component) {
+                            initialise(component);
+                        });
+                    }
+                });
+            };
+        }
+
+        /**
+         * Renders sub dialog inside the container specified for the given component path
+         * @param container         {HTMLElement}    container on which the dialog is to be rendered
+         * @param dialogPath        {String}        resource type of the dialog
+         */
+        static renderSubDialog(container) {
+            return function(dialogPath, callback) {
+                var args = Array.prototype.slice.call(arguments);
+                var $container = $(container);
+                var componentPath = $container.data("componentpath");
+                var isDialogRendered = false;
+                if (componentPath && dialogPath) {
+                    $container.empty();
+                    var currentDialogPath = dialogPath + "/cq:dialog.html";
+                    if (currentDialogPath.indexOf("/") !== 0) {
+                        currentDialogPath = "/mnt/overlay/" + currentDialogPath;
+                    }
+                    var actionPath = currentDialogPath + componentPath;
+                    var subDialogResponse = CQ.shared.HTTP.get(actionPath);
+                    if (CQ.shared.HTTP.isOk(subDialogResponse)) {
+                        var parser = $(window).adaptTo("foundation-util-htmlparser"),
+                            html = subDialogResponse.body;
+                        parser.parse(html, true).then(function (dialogHtml) {
+                            var $subDialogContent = $(dialogHtml).find('.cq-dialog-content');
+                            $subDialogContent.addClass("guide-dialog");
+                            if ($subDialogContent.length > 0) {
+                                isDialogRendered = true;
+                                $container.append($subDialogContent);
+                                $subDialogContent.trigger('foundation-contentloaded');
+                                callback(isDialogRendered);
+                            }
+                        });
+                    }
+                }
+                callback(isDialogRendered);
+            };
+        }
+
+    }
+
+})(jQuery, jQuery(document), Coral);
