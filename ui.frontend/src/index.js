0702b8f57daba063b4d2a74ea31fdb3b761718fd
nit23uec <niting.iitr@gmail.com>
2022-06-17 14:05:15 +0530

    Use async/await

--- ui.frontend/src/index.js
+++ ui.frontend/src/index.js
@@ -21,20 +21,19 @@ var selectors = {
     self: "[data-" + NS + '-is="' + IS + '"]'
 };
 
-function onDocumentReady() {
+
+async function onDocumentReady() {
     var elements = document.querySelectorAll(selectors.self);
     for (let i = 0; i < elements.length; i++) {
         let dataset = readData(elements[i], IS);
         let containerPath = dataset["path"];
-        $.getJSON(containerPath + ".model.json", function(formJson) {
-            console.log("model json from server ", formJson);
-            createFormInstance(formJson, function (formModel) {
-                console.log("AF2.0 model initialised", formModel);
-                window.af.formsRuntime.view.formContainer[containerPath]  = new FormContainer({_model: formModel, path: containerPath});
-                const event = new CustomEvent("FormContainerInitialised", { "detail": containerPath });
-                document.dispatchEvent(event);
-            });
-        });
+        let formJson = await $.getJSON(containerPath + ".model.json");
+        console.log("model json from server ", formJson);
+        var formModel = await createFormInstance(formJson);
+        console.log("AF2.0 model initialised", formModel);
+        window.af.formsRuntime.view.formContainer[containerPath]  = new FormContainer({_model: formModel, path: containerPath});
+        const event = new CustomEvent("FormContainerInitialised", { "detail": containerPath });
+        document.dispatchEvent(event);
     }
 }
 
