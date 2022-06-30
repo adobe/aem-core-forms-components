45bc568a00611fc5e4c2e7ecda0840c63d67d769
nit23uec <niting.iitr@gmail.com>
2022-06-30 21:21:14 +0530

    common webpack config

--- ui.frontend/webpack.common.js
+++ ui.frontend/webpack.common.js
@@ -13,14 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
 const path = require('path');
 
-module.exports = () => {
-    return merge(common, {
-        mode: 'development',
-        devtool: 'source-map'
-    });
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        library: {
+            name: 'FormView',
+            type: 'window'
+        },
+    },
 };
--- ui.frontend/webpack.prod.js
+++ ui.frontend/webpack.prod.js
@@ -13,18 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        library: {
-            name: 'FormView',
-            type: 'window'
-        },
-    },
+module.exports = () => {
+    return merge(common, {
+        mode: 'production',
+    });
 };
