cd0cfea5d73c8d1dfef7af474dba5744f1d95f19
Rishi Mehta <rismehta@adobe.com>
2022-08-29 19:45:56 +0530

    @trivial Fixing flaky test

--- ui.tests/test-module/libs/support/commands.js
+++ ui.tests/test-module/libs/support/commands.js
@@ -152,18 +152,6 @@ Cypress.Commands.add("openSiteAuthoring", (pagePath) => {
 // Cypress command to open authoring page
 Cypress.Commands.add("openAuthoring", (pagePath) => {
     const baseUrl = Cypress.env('crx.contextPath') ?  Cypress.env('crx.contextPath') : "";
-    // this event will automatically be unbound when this
-    // test ends because it's attached to 'cy'
-    cy.on('uncaught:exception', (err, runnable) => {
-        expect(err.message).to.include('Page info could not be');
-        // using mocha's async done callback to finish
-        // this test so we prove that an uncaught exception
-        // was thrown
-        done();
-        // return false to prevent the error from
-        // failing this test
-        return false;
-    });
     cy.visit(baseUrl);
     cy.login(baseUrl);
     cy.openSiteAuthoring(pagePath);
@@ -223,7 +211,7 @@ Cypress.Commands.add("openEditableToolbar", (selector) => {
 
 // cypress command to invoke an editable action
 Cypress.Commands.add("invokeEditableAction", (actionSelector) => {
-    cy.get(actionSelector).trigger("click");
+    cy.get(actionSelector).should('be.visible').click({force: true});
 });
 
 // cypress command to initialize event handler on channel
