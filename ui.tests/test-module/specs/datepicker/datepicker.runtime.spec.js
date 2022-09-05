bd0b03be2b5bb7e38e4546a47212577c87b52155
Varun Dua <varundua16@gmail.com>
2022-09-05 13:14:05 +0530

    Datepicker (#137)

---
+++ ui.tests/test-module/specs/datepicker/datepicker.runtime.spec.js
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright 2022 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+describe.skip("Form with Datepicker", () => {
+
+    const pagePath = "content/forms/af/core-components-it/samples/datepicker/basic.html"
+    const bemBlock = 'cmp-adaptiveform-datepicker'
+
+    let formContainer = null
+
+    beforeEach(() => {
+        cy.previewForm(pagePath).then(p => {
+            formContainer = p;
+        })
+    });
+
+    const checkHTML = (id, state) => {
+        const visible = state.visible
+        const passVisibleCheck = `${visible === true ? "" : "not."}be.visible`
+        const passHiddenAttributeCheck = `${visible === false ? "" : "not."}have.attr`
+        const passDisabledAttributeCheck = `${state.enabled === false ? "" : "not."}have.attr`;
+        const value = state.value
+        cy.get(`#${id}`)
+            .should(passVisibleCheck)
+            .should(passHiddenAttributeCheck, `data-${bemBlock}-hidden`);
+        cy.get(`#${id}`).should(passDisabledAttributeCheck, `data-${bemBlock}-disabled`);
+        return cy.get(`#${id}`).within((root) => {
+            cy.get('*').should(passVisibleCheck)
+            cy.get('input')
+                .should(passDisabledAttributeCheck, 'disabled');
+            cy.get('input').should('have.value', value)
+        })
+    }
+
+    it(" should get model and view initialized properly ", () => {
+        expect(formContainer, "formcontainer is initialized").to.not.be.null;
+        expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
+        Object.entries(formContainer._fields).forEach(([id, field]) => {
+            expect(field.getId()).to.equal(id)
+            expect(formContainer._model.getElement(id), `model and view are in sync`).to.equal(field.getModel())
+        });
+    })
+
+    it(" model's changes are reflected in the html ", () => {
+        const [id, fieldView] = Object.entries(formContainer._fields)[0]
+        const model = formContainer._model.getElement(id)
+        model.value = "2020-10-10"
+        checkHTML(model.id, model.getState()).then(() => {
+            model.visible = false
+            return checkHTML(model.id, model.getState())
+        }).then(() => {
+            model.enable = false
+            return checkHTML(model.id, model.getState())
+        })
+    });
+
+    it(" html changes are reflected in model ", () => {
+        const [id, fieldView] = Object.entries(formContainer._fields)[0]
+        const model = formContainer._model.getElement(id)
+        const input = "2020-10-10"
+        cy.get(`#${id}`).find("input").clear().type(input).blur().then(x => {
+            expect(model.getState().value).to.equal(input)
+        })
+    });
+
+})
