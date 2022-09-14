4291cb7c2ba18d55936ecc924518164882a7c961
Shrikha <smahanty@adobe.com>
2022-09-14 15:12:47 +0530

    few fixes

--- ui.apps/src/main/content/jcr_root/apps/core/fd/components/form/datepicker/v1/datepicker/clientlibs/editor/js/editDialog.js
+++ ui.apps/src/main/content/jcr_root/apps/core/fd/components/form/datepicker/v1/datepicker/clientlibs/editor/js/editDialog.js
@@ -17,69 +17,9 @@
     "use strict";
 
     var EDIT_DIALOG = ".cmp-adaptiveform-datepicker__editdialog",
-        DATEPICKER_ALLOWRICHTEXT = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__allowrichtext",
-        DATEPICKER_MAXCHARS = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__maxchars",
-        DATEPICKER_MINLENGTH = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__minlength",
-        BASE_PLACEHOLDER = EDIT_DIALOG + " .cmp-adaptiveform-base__placeholder",
-        DATEPICKER_VALUE = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__value",
-        DATEPICKER_RICHTEXTVALUE = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__richtextvalue",
-        DATEPICKER_AUTOCOMPLETE = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__autocomplete",
-        DATEPICKER_AUTOFILL_FIELD_KEYWORD = EDIT_DIALOG + " .cmp-adaptiveform-datepicker__autofillfieldkeyword",
         Utils = window.CQ.FormsCoreComponents.Utils.v1;
 
 
-    /**
-     * Toggles the visibility of the maxChars, minLength, placeholder field based on the checked state of
-     * the allowRichText checkbox
-     * @param {HTMLElement} dialog The dialog on which the operation is to be performed.
-     */
-    function handleRichText(dialog) {
-        var component = dialog.find(DATEPICKER_ALLOWRICHTEXT)[0];
-        var datePickerMaxChars = dialog.find(DATEPICKER_MAXCHARS);
-        var datePickerMinLength = dialog.find(DATEPICKER_MINLENGTH);
-        var basePlaceHolder = dialog.find(BASE_PLACEHOLDER).parent('div');
-        var datePickerValue = dialog.find(DATEPICKER_VALUE);
-        var datePickerRichTextValue = dialog.find(DATEPICKER_RICHTEXTVALUE);
-        var listOfElements = [datePickerMaxChars, datePickerMinLength, basePlaceHolder, datePickerValue];
-
-        var isNotChecked = function() {return !isChecked()};
-        var isChecked = function() {return component.checked};
-        var hideAndShowElements = function() {
-            // hide other elements
-            Utils.checkAndDisplay(listOfElements)(isNotChecked);
-            // show rich text
-            Utils.checkAndDisplay(datePickerRichTextValue)(isChecked);
-        };
-        hideAndShowElements();
-        component.on("change", function() {
-            hideAndShowElements();
-        });
-        var changeFormFields = Utils.manipulateNameAndValue([datePickerValue[0], datePickerRichTextValue[0]]);
-        if (isChecked()) {
-            var richTextContainer = datePickerRichTextValue.parent('.richtext-container');
-            var richTextEditable = richTextContainer.find(".cq-RichText-editable");
-            var filteredValue = Utils.encodeScriptableTags(datePickerValue[0].value);
-            richTextEditable.empty().append(filteredValue);
-            changeFormFields(["./_plainTextValue@Delete", "./_value"], [null, filteredValue]);
-        } else {
-            //Removing html tags from content and setting it to default text field
-            var filteredValue =  $('<div>').html(datePickerValue[0].value).text();
-            changeFormFields(["./_value", "./_richTextValue@Delete"], [filteredValue, null]);
-        }
-    }
-
-    function handleAutoComplete(dialog) {
-        var datePickerAutoComplete = dialog.find(DATEPICKER_AUTOCOMPLETE)[0];
-        var datePickerAutofill = dialog.find(DATEPICKER_AUTOFILL_FIELD_KEYWORD);
-        var isChecked = function() {return datePickerAutoComplete.checked};
-        var hideAndShowElements = function() {
-            Utils.checkAndDisplay(datePickerAutofill)(isChecked);
-        };
-        hideAndShowElements();
-        datePickerAutoComplete.on("change", function() {
-            hideAndShowElements();
-        });
-    }
-    Utils.initializeEditDialog(EDIT_DIALOG)(handleAutoComplete, handleRichText);
+    Utils.initializeEditDialog(EDIT_DIALOG)();
 
 })(jQuery);
