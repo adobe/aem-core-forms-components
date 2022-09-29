3a42a68e58dd4af4c70f61d39478f86e0de667c6
vdua <vdua@adobe.com>
2022-09-29 17:58:20 +0530

    CQ-4348321 Hidden fields are disabled in the runtime
    - Adding failing test cases

--- ui.tests/test-module/specs/textinput/textinput.runtime.spec.js
+++ ui.tests/test-module/specs/textinput/textinput.runtime.spec.js
@@ -34,7 +34,7 @@ describe("Form Runtime with Text Input", () => {
         const visible = state.visible;
         const passVisibleCheck = `${visible === true ? "" : "not."}be.visible`;
         const passDisabledAttributeCheck = `${state.enabled === false ? "" : "not."}have.attr`;
-        const value = state.value
+        const value = state.value == null ? '' : state.value;
         cy.get(`#${id}`)
             .should(passVisibleCheck)
             .invoke('attr', 'data-cmp-visible')
@@ -56,6 +56,7 @@ describe("Form Runtime with Text Input", () => {
         Object.entries(formContainer._fields).forEach(([id, field]) => {
             expect(field.getId()).to.equal(id)
             expect(formContainer._model.getElement(id), `model and view are in sync`).to.equal(field.getModel())
+            checkHTML(id, field.getModel().getState())
         });
     })
 
