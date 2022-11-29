6e766c30643f945029a211cb52eaa0eea70af9e4
ujjgupta <ujjgupta@adobe.com>
2022-11-29 16:05:27 +0530

    CQ-4350282 Removing toggle bundle which is internal to adobe

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -49,7 +49,7 @@ try {
     // Start CQ
     ci.sh(`./qp.sh -v start --id author --runmode author --port 4502 --qs-jar /home/circleci/cq/author/cq-quickstart.jar \
             --bundle org.apache.sling:org.apache.sling.junit.core:1.0.23:jar \
-            --install-file ${config.modules['core-forms-components-it-tests-apps', 'core-forms-components-it-tests-content'].path}/src/main/resources/com.adobe.granite.toggle.impl.dev-1.1.2.jar \
+            --install-file ${config.modules['core-forms-components-it-tests-apps', 'core-forms-components-it-tests-content'].path} \
             --bundle com.adobe.cq:core.wcm.components.examples.all:${wcmVersion}:zip \
             ${extras} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-apps'])} \
