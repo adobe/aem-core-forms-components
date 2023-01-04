324249ef01fe2532832ab95a7b58bc2abad389b7
Ravisanker E <ravisanker1999@gmail.com>
2023-01-04 19:07:15 +0530

    Update package-info.java

--- it/core/src/main/java/com/adobe/cq/forms/core/components/it/servlets/package-info.java
+++ it/core/src/main/java/com/adobe/cq/forms/core/components/it/servlets/package-info.java
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-@Version("1.0")
+@Version("1.0.0")
 package com.adobe.cq.forms.core.components.it.servlets;
 
 import org.osgi.annotation.versioning.Version;
