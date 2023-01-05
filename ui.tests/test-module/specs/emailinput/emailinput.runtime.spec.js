028aaf7c0cec42b8de69dd68007b94cace4b614a
Gulshan Mishra <egmox2@gmail.com>
2023-01-05 11:37:16 +0530

    CQ 4349496 Email component (#328)

---
+++ ui.tests/test-module/specs/emailinput/emailinput.runtime.spec.js
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright 2022 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+describe("Form Runtime with Email Input", () => {
+
+    const pagePath = "content/forms/af/core-components-it/samples/emailinput/basic.html"
+    const bemBlock = 'cmp-adaptiveform-emailinput'
+    const IS = "adaptiveFormEmailInput"
+    const selectors = {
+        textinput : `[data-cmp-is="${IS}"]`
+    }
+
+    let formContainer = null
+
+    beforeEach(() => {
+        cy.previewForm(pagePath).then(p => {
+            formContainer = p;
+        })
+    });
+
+    const checkHTML = (id, state) => {
+        const visible = state.visible;
+        const passVisibleCheck = `${visible === true ? "" : "not."}be.visible`;
+        const passDisabledAttributeCheck = `${state.enabled === false ? "" : "not."}have.attr`;
+        const value = state.value == null ? '' : state.value;
+        cy.get(`#${id}`)
+            .should(passVisibleCheck)
+            .invoke('attr', 'data-cmp-visible')
+            .should('eq', visible.toString());
+        cy.get(`#${id}`)
+            .invoke('attr', 'data-cmp-enabled')
+            .should('eq', state.enabled.toString());
+        return cy.get(`#${id}`).within((root) => {
+            cy.get('*').should(passVisibleCheck)
+            cy.get('input')
+                .should(passDisabledAttributeCheck, 'disabled');
+            cy.get('input').should('have.value', value)
+        })
+    }
+
+    it(" should get model and view initialized properly ", () => {
+        expect(formContainer, "formcontainer is initialized").to.not.be.null;
+        expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
+        Object.entries(formContainer._fields).forEach(([id, field]) => {
+            expect(field.getId()).to.equal(id)
+            expect(formContainer._model.getElement(id), `model and view are in sync`).to.equal(field.getModel())
+            checkHTML(id, field.getModel().getState())
+        });
+    })
+
+    it(" model's changes are reflected in the html ", () => {
+        const [id, fieldView] = Object.entries(formContainer._fields)[0]
+        const model = formContainer._model.getElement(id)
+        model.value = "some other value"
+        checkHTML(model.id, model.getState()).then(() => {
+            model.visible = false
+            return checkHTML(model.id, model.getState())
+        }).then(() => {
+            model.enabled = false
+            return checkHTML(model.id, model.getState())
+        })
+    });
+
+    it(" html changes are reflected in model ", () => {
+        const [id, fieldView] = Object.entries(formContainer._fields)[0]
+        const model = formContainer._model.getElement(id)
+        const input = "value"
+        cy.get(`#${id}`).find("input").clear().type(input).blur().then(x => {
+            expect(model.getState().value).to.equal(input)
+        })
+    });
+
+    it("should toggle description and tooltip", () => {
+        cy.toggleDescriptionTooltip(bemBlock, 'tooltip_scenario_test');
+    })
+
+})
