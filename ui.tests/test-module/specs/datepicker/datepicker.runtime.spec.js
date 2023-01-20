baecc9976fc5e4639e4b07ac07242cd0ccf39608
Talmiz Ahmed <talmiz.tech@gmail.com>
2023-01-20 15:40:01 +0530

    Datepicker conflicting ctest collateral fix

--- ui.tests/test-module/specs/datepicker/datepicker.runtime.spec.js
+++ ui.tests/test-module/specs/datepicker/datepicker.runtime.spec.js
@@ -128,16 +128,16 @@ describe("Form Runtime with Date Picker", () => {
         })
     })
 
-    it("should set and clear value", () => {
-        // Rule on datePicker5: When input of datePicker5 is '2023-01-12', set value of datePicker4 to '2023-01-01' and clear value of datePicker1
+    it("should set and clear value based on rules", () => {
+        // Rule on datePicker6: When input of datePicker6 is '2023-01-12', set value of datePicker4 to '2023-01-01' and clear value of datePicker1
 
         const [datePicker1, datePicker1FieldView] = Object.entries(formContainer._fields)[0];
         const [datePicker4, datePicker4FieldView] = Object.entries(formContainer._fields)[3];
-        const [datePicker5, datePicker5FieldView] = Object.entries(formContainer._fields)[4];
+        const [datePicker6, datePicker6FieldView] = Object.entries(formContainer._fields)[5];
 
         const input = "2023-01-12";
         cy.get(`#${datePicker1}`).find("input").clear().type('2022-05-18')
-        cy.get(`#${datePicker5}`).find("input").clear().type(input).blur().then(x => {
+        cy.get(`#${datePicker6}`).find("input").clear().type(input).blur().then(x => {
             cy.get(`#${datePicker1}`).find("input").should('have.value', "")
             cy.get(`#${datePicker4}`).find("input").should('have.value', "2023-01-01")
         })
