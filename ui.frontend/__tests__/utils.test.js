27e0ff9a3b17416d3ba906b0d99fd506da93cdfa
Sakshi Arora <sakarora@Sakshis-MacBook-Pro.local>
2023-02-08 12:19:59 +0530

    Increasing the af-core version

--- ui.frontend/__tests__/utils.test.js
+++ ui.frontend/__tests__/utils.test.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-import Utils from "../src/utils.js";
+import Utils from "../src/utils";
 
 test('readData test', () => {
     let element = {
