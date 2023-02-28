3112e89f6c751a52017e661c1f04cf715aea9620
Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-4.macromedia.com>
2023-02-28 08:48:57 +0530

    [CQ-4348351] added private fields in tabs view and constants for id suffixes as suggested in review....

--- ui.tests/test-module/specs/tabsontop/tabsontop.runtime.repeatability.spec.js
+++ ui.tests/test-module/specs/tabsontop/tabsontop.runtime.repeatability.spec.js
@@ -46,7 +46,7 @@ describe("Form with TabsOnTop Container", () => {
         cy.get("button").contains("+R1").click().then(() => {
             getTabs().should('have.length', 5);
             getTabPanels().should('have.length', 5);
-            cy.get("button").contains("-R1").click().then(() =>{
+            cy.get("button").contains("-R1").click().then(() => {
                 getTabs().should('have.length', 4);
                 getTabPanels().should('have.length', 4);
                 cy.get("button").contains("-R1").click().then(() => {
@@ -60,11 +60,11 @@ describe("Form with TabsOnTop Container", () => {
     it("testing max instance addition", () => {
         getTabs().should('have.length', 4);
         getTabPanels().should('have.length', 4);
-        for(let i=0;i<4;i++){
+        for (let i = 0; i < 4; i++) {
             cy.get("button").contains("+R1").click().then(() => {
-                const maxOccurAllowed=4;
-                const initialNoOfTabs=4;
-                const length = i<maxOccurAllowed-1 ? initialNoOfTabs+(i+1): 7;   // panel is allowed max occurence of 4
+                const maxOccurAllowed = 4;
+                const initialNoOfTabs = 4;
+                const length = i < maxOccurAllowed - 1 ? initialNoOfTabs + (i + 1) : 7;   // panel is allowed max occurence of 4
                 getTabs().should('have.length', length);
                 getTabPanels().should('have.length', length);
             })
@@ -77,55 +77,55 @@ describe("Form with TabsOnTop Container", () => {
         cy.get("button").contains("+R1").click().then(() => {
             getTabs().should('have.length', 5);
             getTabPanels().should('have.length', 5);
-            getTabAtIndex(1).should('have.attr','aria-controls');
-            getTabAtIndex(1).should('have.attr','id');
-            getTabAtIndex(1).should('have.class','cmp-tabs__tab--active');
-            getTabPanelAtIndex(1).should('have.attr','id');
-            getTabPanelAtIndex(1).should('have.attr','aria-labelledby');
-            getTabPanelAtIndex(1).should('have.class','cmp-tabs__tabpanel--active');
+            getTabAtIndex(1).should('have.attr', 'aria-controls');
+            getTabAtIndex(1).should('have.attr', 'id');
+            getTabAtIndex(1).should('have.class', 'cmp-tabs__tab--active');
+            getTabPanelAtIndex(1).should('have.attr', 'id');
+            getTabPanelAtIndex(1).should('have.attr', 'aria-labelledby');
+            getTabPanelAtIndex(1).should('have.class', 'cmp-tabs__tabpanel--active');
         })
     })
 
-    it("test addedPanel position when inserted at first position in tab",() =>{
+    it("test addedPanel position when inserted at first position in tab", () => {
         getTabs().should('have.length', 4);
         getTabPanels().should('have.length', 4);
-        getTabAtIndex(0).should('have.class','cmp-tabs__tab--active');
-        getTabPanelAtIndex(0).should('have.class','cmp-tabs__tabpanel--active');
-        getTabAtIndex(1).should('not.have.class','cmp-tabs__tab--active');
-        getTabPanelAtIndex(1).should('not.have.class','cmp-tabs__tabpanel--active');
+        getTabAtIndex(0).should('have.class', 'cmp-tabs__tab--active');
+        getTabPanelAtIndex(0).should('have.class', 'cmp-tabs__tabpanel--active');
+        getTabAtIndex(1).should('not.have.class', 'cmp-tabs__tab--active');
+        getTabPanelAtIndex(1).should('not.have.class', 'cmp-tabs__tabpanel--active');
         getTabPanelAtIndex(1).find('.cmp-adaptiveform-numberinput');
-        cy.get("button").contains("-R1").click().then(() =>{
+        cy.get("button").contains("-R1").click().then(() => {
             getTabs().should('have.length', 3);
             getTabPanels().should('have.length', 3);
             //check panel which was at first index is now at zero
             getTabPanelAtIndex(0).find('.cmp-adaptiveform-numberinput');
-            cy.get("button").contains("+R1").click().then(() =>{
+            cy.get("button").contains("+R1").click().then(() => {
                 //check that the panel is indeed added at 0th index and is active
                 getTabPanelAtIndex(0).find('.cmp-adaptiveform-textinput');
-                getTabAtIndex(0).should('have.class','cmp-tabs__tab--active');
-                getTabPanelAtIndex(0).should('have.class','cmp-tabs__tabpanel--active');
+                getTabAtIndex(0).should('have.class', 'cmp-tabs__tab--active');
+                getTabPanelAtIndex(0).should('have.class', 'cmp-tabs__tabpanel--active');
             })
         })
     })
 
-    it("test addedPanel position when inserted at first position in instanceManager",() =>{
+    it("test addedPanel position when inserted at first position in instanceManager", () => {
         getTabs().should('have.length', 4);
         getTabPanels().should('have.length', 4);
-        getTabAtIndex(2).should('not.have.class','cmp-tabs__tab--active');
-        getTabPanelAtIndex(2).should('not.have.class','cmp-tabs__tabpanel--active');
+        getTabAtIndex(2).should('not.have.class', 'cmp-tabs__tab--active');
+        getTabPanelAtIndex(2).should('not.have.class', 'cmp-tabs__tabpanel--active');
         getTabPanelAtIndex(2).find('.cmp-adaptiveform-datepicker');
         //remove the instance of panel
-        cy.get("button").contains("-R2").click().then(() =>{
+        cy.get("button").contains("-R2").click().then(() => {
             getTabs().should('have.length', 3);
             getTabPanels().should('have.length', 3);
             // check that panel instance gpr removed
             getTabPanelAtIndex(2).find('.cmp-adaptiveform-dropdown');
             //add instance of repeated panel
-            cy.get("button").contains("+R2").click().then(() =>{
+            cy.get("button").contains("+R2").click().then(() => {
                 //check that the panel is indeed added again at the 2nd index and is active
                 getTabPanelAtIndex(2).find('.cmp-adaptiveform-datepicker');
-                getTabAtIndex(2).should('have.class','cmp-tabs__tab--active');
-                getTabPanelAtIndex(2).should('have.class','cmp-tabs__tabpanel--active');
+                getTabAtIndex(2).should('have.class', 'cmp-tabs__tab--active');
+                getTabPanelAtIndex(2).should('have.class', 'cmp-tabs__tabpanel--active');
             })
 
         })
