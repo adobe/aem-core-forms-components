b3a927ccc70026064e45101d76d6b9ceacf00112
dgurjar <dgurjar@adobe.com>
2023-03-01 18:22:54 +0530

    FORMS-7154 Rich text editor is not supported on text-input in core component based forms

--- ui.tests/test-module/specs/textinput/textinput.authoring.spec.js
+++ ui.tests/test-module/specs/textinput/textinput.authoring.spec.js
@@ -54,10 +54,9 @@ describe('Page - Authoring', function () {
     .should("exist");
 
     // Checking some dynamic behaviours
-    cy.get("[name='./allowRichText'][type=\"checkbox\"]").should("exist").check();
-    cy.get(".cmp-adaptiveform-textinput__maxlength").invoke('css', 'display').should('equal','none');
-    cy.get(".cmp-adaptiveform-textinput__minlength").invoke('css', 'display').should('equal','none');
-    cy.get(".cmp-adaptiveform-base__placeholder").parent('div').invoke('css', 'display').should('equal','none');
+    cy.get(".cmp-adaptiveform-textinput__maxlength").invoke('css', 'display').should('equal','block');
+    cy.get(".cmp-adaptiveform-textinput__minlength").invoke('css', 'display').should('equal','block');
+    cy.get(".cmp-adaptiveform-base__placeholder").parent('div').invoke('css', 'display').should('equal','block');
     cy.get('.cq-dialog-cancel').click();
     cy.deleteComponentByPath(textInputDrop);
   }
