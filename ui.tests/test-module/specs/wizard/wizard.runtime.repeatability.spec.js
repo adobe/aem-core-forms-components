d805c50c2abf818cd9ff700bde003a0d8b916752
Deep Prakash Dewanji <deepprakash345@gmail.com>
2023-03-27 16:01:26 +0530

    [FORMS-7489] implemented repeatability in accordion, responsiveGrid (#533)
    * [FORMS-7489] implemented repeatability in accordion, removed duplication of ids in accordion on repeatability, added extensive test cases for repeatability and normal accordion functionality....
    
    * [FORMS-7489] implemented repeatability in accordion, removed duplication of ids in accordion on repeatability, added extensive test cases for repeatability and normal accordion functionality....
    
    * [FORMS-7489] implemented repeatability in accordion, removed duplication of ids in accordion on repeatability, added extensive test cases for repeatability and normal accordion functionality....
    
    * [FORMS-7489] added repeatability in responsivegrid layout as well. centralized common code and added extensive cypress tests for repeatability.
    
    * [FORMS-7489] updated year in license........
    
    * [FORMS-7489] fixed test failure reasons.......
    
    ---------
    
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-4.macromedia.com>
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-MacBook-Pro-4.local>

--- ui.tests/test-module/specs/wizard/wizard.runtime.repeatability.spec.js
+++ ui.tests/test-module/specs/wizard/wizard.runtime.repeatability.spec.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-describe("Form with TabsOnTop Container", () => {
+describe("Form with Wizard Container", () => {
     const pagePath = "content/forms/af/core-components-it/samples/wizard/repeatability.html";
     const bemBlock = 'cmp-tabs';
     let formContainer = null;
