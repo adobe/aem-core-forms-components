bed9731f59fa3a1cafdbb9691bfa57ef27befe8f
Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-4.macromedia.com>
2023-03-28 15:35:11 +0530

    [FORMS-7489] used public api items in place of _children in tests

--- ui.tests/test-module/specs/prefill/repeatableprefillwithzerooccurrencefortabaccordionwizard.spec.js
+++ ui.tests/test-module/specs/prefill/repeatableprefillwithzerooccurrencefortabaccordionwizard.spec.js
@@ -24,16 +24,16 @@ describe("Form with Wizard/Tab/Accordion and Prefill with Zero occurance", () =>
     })
 
     it("test that repeatable instances with zero occurance is not in the dom", () => {
-        const children = formContainer.getModel()._children;
+        const children = formContainer.getModel().items;
         expect(children.length).to.be.equal(3);
         for (let i = 0; i < children.length; i++) {
             let currChild = children[i];
             //model of tab, wizard and accordion has two child each
-            expect(currChild._children.length).to.be.equal(2);
+            expect(currChild.items.length).to.be.equal(2);
             //2nd children of tab, wizard and accordion are repeatable with minOccur equal to zero
-            expect(currChild._children[1].minOccur).to.be.equal(0);
+            expect(currChild.items[1].minOccur).to.be.equal(0);
             //2nd children of  tab, wizard and accordion should not be present in dom.
-            cy.get('#' + currChild._children[1]._jsonModel.items[0].id).should('not.exist');
+            cy.get('#' + currChild.items[1]._jsonModel.items[0].id).should('not.exist');
         }
     })
 })
