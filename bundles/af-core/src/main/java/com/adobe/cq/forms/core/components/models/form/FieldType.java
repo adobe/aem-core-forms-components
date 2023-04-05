484effda5fd2b33bf97e8b0630cfd5321a6d4ba1
Rishi Mehta <69448117+rismehta@users.noreply.github.com>
2023-04-05 14:25:42 +0530

    @trivial Fixing flaky test (#559)
    FORMS-2980: updated fieldType. synced with af2-webruntime
    
    FORMS-2980: dummy change to rerun pipeline

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/models/form/FieldType.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/models/form/FieldType.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum FieldType {
     TEXT_INPUT("text-input"),
-    EMAIL_INPUT("email-input"),
+    EMAIL("email"),
     MULTILINE_INPUT("multiline-input"),
     NUMBER_INPUT("number-input"),
     DATE_INPUT("date-input"),
