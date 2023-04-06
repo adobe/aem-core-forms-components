39d0fb6316d48192686cf3ef8807088b887ada38
Rishi Mehta <rismehta@adobe.com>
2023-04-06 12:50:40 +0530

    Revert "Merge spa branch to dev (#530)"
    This reverts commit 78daf5e0565495fbcd696d2fdb076d9c318e3f32.

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-(function ($, ns) {
+(function ($) {
 
     "use strict";
     window.CQ = window.CQ || {};
@@ -49,26 +49,10 @@
     }
 
     function getFormContainerPath(editable) {
-        let path = editable.dom.find("[data-cmp-adaptiveformcontainer-path]").data("cmpAdaptiveformcontainerPath");
-        if (typeof path !== 'string' || !path) {
-            path = getFormContainerFromDom(editable);
-
-        }
-        return path;
-    }
-
-    function getFormContainerFromDom(editable) {
-        let selector = "[data-cmp-is='adaptiveFormContainer']";
-        let elem = $(editable.dom[0]).closest(selector);
-        let path = null;
-        if (elem.length > 0) {
-            path = elem.data("cmp-path");
-        }
-        return path;
+        return editable.dom.find("[data-cmp-adaptiveformcontainer-path]").data("cmpAdaptiveformcontainerPath");
     }
 
     function getFieldId(editable) {
-        // todo: dead code, not used
         return editable.dom.find("[data-cmp-adaptiveformcontainer-path]").attr('id');
     }
 
@@ -77,4 +61,4 @@
         ui.alert(Granite.I18n.get('Information'), Granite.I18n.get('Please initialise the component to open the rule editor'), 'notice');
     }
 
-})(jQuery, Granite.author);
+})(jQuery);
