87fc44d9d9e48bd568bbbcbd544b9cf3cdc74d47
ujjgupta <ujjgupta@adobe.com>
2023-04-26 13:03:07 +0530

    FORMS-8144 @trivial Fixing data schema tests

--- ui.tests/test-module/specs/formcontainer.spec.js
+++ ui.tests/test-module/specs/formcontainer.spec.js
@@ -63,16 +63,6 @@ describe('Page/Form Authoring', function () {
         cy.get('.cmp-adaptiveform-container'+'__editdialog').contains('Submission').click({force:true});
         cy.get("[name='./actionType']").should("exist");
 
-        //support not present in 6.5 yet
-        //select email submit action
-        /*cy.get(".cmp-adaptiveform-container__submitaction button").click();
-        cy.get("coral-selectlist-item").contains('Send email').should('be.visible').click({force: true});
-        cy.get("[name='./useExternalEmailTemplate']").should("exist");
-        cy.get("[name='./templatePath']").should("exist");
-
-        cy.get("[name='./useExternalEmailTemplate']").should("exist").first().click();
-        cy.get("[name='./template']").should("exist");*/
-
         //select rest endpoint submit action
         cy.get(".cmp-adaptiveform-container__submitaction button").click();
         cy.get("coral-selectlist-item").contains('Submit to REST endpoint').click({force: true});
@@ -114,11 +104,12 @@ describe('Page/Form Authoring', function () {
 
         //click save without selecting the fdm model, error should be displayed
         cy.get(".cq-dialog-submit").click();
-        cy.get(".coral-Form-errorlabel").should("be.visible");
+        cy.get(".coral-Form-fielderror").should("be.visible");
 
         //select fdm and save it
         cy.get(".cmp-adaptiveform-container__fdmselector").should("be.visible").click();
-        cy.get("coral-selectlist-item[value='/content/dam/formsanddocuments-fdm/portal-unified-storage-form-data-model']").contains('Portal Unified Storage Form Data Model').should('be.visible').click();
+        cy.get("coral-selectlist-item[value='/content/dam/formsanddocuments-fdm/ms-dynamics-fdm']").contains('Microsoft Dynamics FDM').should('be.visible').click();
+
         cy.get(".cq-dialog-submit").click();
     };
 
@@ -132,9 +123,13 @@ describe('Page/Form Authoring', function () {
 
         //since data model is already selected it should be disabled
         cy.get(".cmp-adaptiveform-container__selectformmodel").should("not.have.attr", "disabled");
-        cy.get(".cmp-adaptiveform-container__fdmselector").click();
+        cy.get(".cmp-adaptiveform-container__selectformmodel").click();
 
-        cy.get("coral-selectlist-item[value='/content/dam/formsanddocuments-fdm/forms-ootb-usc-workflow-fdm']").contains('Workflow Unified Storage Form Data Model').should('be.visible').click();
+        //select Schema as data model
+        cy.get("coral-selectlist-item[value='jsonschema']").contains('Schema').should('be.visible').click();
+        cy.get(".cmp-adaptiveform-container__schemaselector").click();
+        //choose a schema
+        cy.get("coral-selectlist-item[value='/content/dam/formsanddocuments/core-components-it/samples/panelcontainer/bank_api.schema.json']").contains('bank_api.schema.json').should('be.visible').click();
         cy.get("#formModelChange").should("be.visible");
         cy.get("#formModelDialogAcceptButton").click();
         cy.get(".cq-dialog-submit").click();
