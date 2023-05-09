47d68806d273e4b17e7bcd63cd886305147d2244
Gulshan Mishra <gulshanm@adobe.com>
2023-05-09 15:02:55 +0530

    FORMS-7896: added basic switch component

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/SwitchImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/SwitchImpl.java
@@ -15,79 +15,56 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.forms.core.components.internal.models.v1.form;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.jetbrains.annotations.NotNull;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.forms.core.components.internal.form.FormConstants;
 import com.adobe.cq.forms.core.components.models.form.Switch;
-import com.adobe.cq.forms.core.components.util.AbstractBaseImpl;
+import com.adobe.cq.forms.core.components.util.AbstractOptionsFieldImpl;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Model(
     adaptables = { SlingHttpServletRequest.class, Resource.class },
-    adapters = { Switch.class, ComponentExporter.class },
+    adapters = { Switch.class,
+        ComponentExporter.class },
     resourceType = { FormConstants.RT_FD_FORM_SWITCH_V1 })
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class SwitchImpl extends AbstractBaseImpl implements Switch {
+public class SwitchImpl extends AbstractOptionsFieldImpl implements Switch {
 
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL, name = "onValue")
-    protected String onValue;
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL, name = "offValue")
-    protected String offValue;
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL, name = "icon")
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL, name = "orientation")
     @Nullable
-    private String icon;
+    protected String orientationJcr;
+    private Orientation orientation;
 
-    @Override
-    public String getOffValue() {
-        return offValue;
+    @PostConstruct
+    private void initSwitchModel() {
+        orientation = Orientation.fromString(orientationJcr);
     }
 
     @Override
-    public String getOnValue() {
-        return onValue;
+    public @NotNull Map<String, Object> getCustomLayoutProperties() {
+        Map<String, Object> customLayoutProperties = super.getCustomLayoutProperties();
+        if (orientation != null) {
+            customLayoutProperties.put("orientation", orientation.getValue());
+        }
+        return customLayoutProperties;
     }
-
-    @Override
-    public String getIcon() {
-        return icon;
-    }
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    @Default(booleanValues = false)
-    protected boolean dorExclusion;
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    @org.jetbrains.annotations.Nullable
-    protected String dorColspan;
 
     @Override
     @JsonIgnore
-    public Map<String, Object> getDorProperties() {
-        Map<String, Object> customDorProperties = new LinkedHashMap<>();
-        customDorProperties.put("dorExclusion", dorExclusion);
-        if (dorColspan != null) {
-            customDorProperties.put("dorColspan", dorColspan);
-        }
-        return customDorProperties;
-    }
-
-    @Override
-    public Map<String, Object> getProperties() {
-        return super.getProperties();
+    public Orientation getOrientation() {
+        return orientation;
     }
 }
