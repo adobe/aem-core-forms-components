222c3842894d1cf86135bf0647efebe4b1e7ea13
Arun Kumar Attri <aattri@adobe.com>
2023-05-26 22:55:09 +0530

    FORMS-6163: updated custom function tests to use latest addon flag

--- ui.tests/test-module/specs/customFunctionTest.spec.js
+++ ui.tests/test-module/specs/customFunctionTest.spec.js
@@ -29,31 +29,34 @@ describe('Form with custom functions configured in client lib', () => {
         })
     });
 
-    it('should have custom function definition loaded in the window object', () => {
-        expect(formContainer, "formcontainer is initialized").to.not.be.null;
-        let func;
-        cy.window().then(win => {
-            func = win.testFunction1;
-            expect(func).to.not.be.null;
-            expect(func).to.not.be.undefined;
+    if (cy.af.isLatestAddon()) {
+        it('should have custom function definition loaded in the window object', () => {
+            expect(formContainer, "formcontainer is initialized").to.not.be.null;
+            let func;
+            cy.window().then(win => {
+                func = win.testFunction1;
+                expect(func).to.not.be.null;
+                expect(func).to.not.be.undefined;
+            })
+
         })
 
-    })
-
-    it("should have custom function registered in FunctionRuntime", () => {
-        expect(formContainer, "formcontainer is initialized").to.not.be.null;
-        let func;
-        cy.window().then(win => {
-            func = win.FormView.FunctionRuntime.customFunctions.testFunction1;
-            expect(func).to.not.be.null;``
-            expect(func).to.not.be.undefined;
+        it("should have custom function registered in FunctionRuntime", () => {
+            expect(formContainer, "formcontainer is initialized").to.not.be.null;
+            let func;
+            cy.window().then(win => {
+                func = win.FormView.FunctionRuntime.customFunctions.testFunction1;
+                expect(func).to.not.be.null;
+                ``
+                expect(func).to.not.be.undefined;
+            })
         })
-    })
 
-    it("should have set textinput value based on custom function rule", () => {
-        // Rule on textBox1: set value of textBox1 to "test", which is output of custom function testFunction1()
-        expect(formContainer, "formcontainer is initialized").to.not.be.null;
-        const [textbox1, textBox1FieldView] = Object.entries(formContainer._fields)[0];
-        cy.get(`#${textbox1}`).find("input").should('have.value', "test")
-    })
+        it("should have set textinput value based on custom function rule", () => {
+            // Rule on textBox1: set value of textBox1 to "test", which is output of custom function testFunction1()
+            expect(formContainer, "formcontainer is initialized").to.not.be.null;
+            const [textbox1, textBox1FieldView] = Object.entries(formContainer._fields)[0];
+            cy.get(`#${textbox1}`).find("input").should('have.value', "test")
+        })
+    }
 })
