ec8464402e735fb2eae5c6c3408fa6ce56dabb2f
ujjgupta <ujjgupta@adobe.com>
2023-06-05 16:53:01 +0530

    Fixing tests for 650 branch

--- ui.tests/test-module/specs/contentfragment/contentfragment.authoring.spec.js
+++ ui.tests/test-module/specs/contentfragment/contentfragment.authoring.spec.js
@@ -92,7 +92,7 @@ describe('Content Fragment - Authoring', function () {
 
         cy.get("[name='./fragmentVariationPath'] input[role='combobox']").should("exist").clear()
             .type("/content/experience-fragments/test-experience-fragment/master", { delay: 0 }).click();
-        cy.get("._coral-Menu-itemLabel:contains(/content/experience-fragments/test-experience-fragment/master)").should("be.visible").click();
+        cy.get(".foundation-picker-buttonlist button").should("be.visible").click();
         cy.get('.cq-dialog-submit').click().then(y => {
             cy.get('.cq-dialog-submit').should('not.exist').then(z => {
                 cy.deleteComponentByPath(xfDrop);
