983d77601c9cc4b63bb86fe21aeda39f285f89e9
arunattri26 <arunattri26@gmail.com>
2023-07-10 12:31:51 +0530

    FORMS-9488: uncomment textinput authoring rule editor test (#731)
    * FORMS-9488: uncomment textinput authoring rule editor test
    
    * FORMS-9488: uncomment textinput authoring rule editor test
    
    * FORMS-9488: uncomment textinput authoring rule editor test
    
    * FORMS-9488: uncomment textinput authoring rule editor test
    
    ---------
    
    Co-authored-by: Arun Kumar Attri <aattri@adobe.com>

--- ui.tests/test-module/specs/textinput/textinput.authoring.spec.js
+++ ui.tests/test-module/specs/textinput/textinput.authoring.spec.js
@@ -144,15 +144,14 @@ describe('Page - Authoring', function () {
       dropTextInputInSites();
       cy.deleteComponentByPath(textInputDrop);
     });
-    
+
     it('open edit dialog of aem forms TextInput', function() {
       testTextInputBehaviour(textInputEditPathSelector, textInputDrop, true);
     });
 
     // conditionally run the test on latest addon
     //if (cy.af.isLatestAddon()) {
-      // todo: needs to be uncommented later
-        it.skip('Test z-index of Rule editor iframe', function () {
+        it('Test z-index of Rule editor iframe', function () {
             dropTextInputInSites();
             cy.openSidePanelTab("Content Tree");
             cy.openEditableToolbar(sitesSelectors.overlays.overlay.component + textInputEditPathSelector);
@@ -171,8 +170,7 @@ describe('Page - Authoring', function () {
 
     // not yet in available publicly released april far
     //if (cy.af.isLatestAddon()) {
-      // todo: needs to be uncommented later
-      it.skip('Test z-index of Rule editor iframe for components inside site container', function () {
+      it('Test z-index of Rule editor iframe for components inside site container', function () {
           cy.openSidePanelTab("Content Tree");
           cy.openEditableToolbar(sitesSelectors.overlays.overlay.component + textInputInsideSitesContainerEditPathSelector);
           cy.invokeEditableAction("[data-action='editexpression']");
