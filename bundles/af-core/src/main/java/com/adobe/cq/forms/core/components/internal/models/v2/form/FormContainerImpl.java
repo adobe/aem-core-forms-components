ae3d68424b6546638d72910855a40abf81dd0a88
tyagiaditi <30658722+tyagiaditi@users.noreply.github.com>
2023-07-17 17:30:51 +0530

    FORMS-8771 - Captcha core component p2 (#734)
    * updating local dependency
    
    * trivial
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P1
    
    * FORMS-8168- Captcha core component - P2
    
    * FORMS-8168- Captcha core component - P2
    
    * FORMS-8168- Captcha core component - P2 modifying view css
    
    * FORMS-8168- Captcha core component - P2 Adding test for model
    
    * Delete package-lock.json
    
    * FORMS-8168- Captcha core component - P2 Adding UI tests
    
    * FORMS-8168- Captcha core component - P2 Removing package.json
    
    * FORMS-8168- Captcha core component - P2 Removing package.json
    
    * FORMS-8168- Captcha core component - P2 Removing package.json
    
    * FORMS-8168- Captcha core component - P2 Removing package.json
    
    * FORMS-8168- Captcha core component - P2 Adding Captcha Class
    
    * FORMS-8168- Captcha core component - P2 Updating recaptcha provider
    
    * FORMS-8168- Captcha core component - P2 Updating model test
    
    * FORMS-8168- Captcha core component - P2 Formatting Captcha class
    
    * FORMS-8168- Captcha core component - Removing from core-component-examples
    
    * FORMS-8168- Fixing Recaptcha Authoring test
    
    * FORMS-8168- Fixing Recaptcha Authoring test
    
    * FORMS-8168- Updating Recaptcha html
    
    * FORMS-8168- Adding cloud config it-content
    
    * FORMS-8168- checkbox test case fix, include clientlib
    
    * FORMS-8168- Adding FT in cy
    
    * FORMS-8168- Resolving conflicts
    
    * FORMS-8168- Resolving conflicts
    
    * Resolve conflicts .content.xml
    
    * FORMS-8168-Replaying
    
    * FORMS-8771- Updating af-core verison
    
    * FORMS-8771- Test visit
    
    * FORMS-8771- Test visit
    
    ---------
    
    Co-authored-by: barshatr <barshatr@adobe.com>

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/form/FormContainerImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/form/FormContainerImpl.java
@@ -292,8 +292,6 @@ public class FormContainerImpl extends AbstractContainerImpl implements FormCont
         }
         return StringUtils.EMPTY;
     }
-<<<<<<< HEAD
-=======
 
     @JsonIgnore
     @Override
@@ -301,6 +299,4 @@ public class FormContainerImpl extends AbstractContainerImpl implements FormCont
         // explicitly setting null, since form container does not have name property, but other containers like panel have a name
         return null;
     }
-
->>>>>>> 56487e6c (FORMS-9364 Changes after PR tests cases failed)
 }
