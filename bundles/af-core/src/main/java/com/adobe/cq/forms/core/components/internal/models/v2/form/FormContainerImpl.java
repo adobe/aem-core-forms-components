56487e6c5af096136a57866d372ea33c0f2e588b
Anurudra Shukla <anurudras@adobe.com>
2023-07-17 11:50:38 +0530

    FORMS-9364 Changes after PR tests cases failed :wq!
    :wq!

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/form/FormContainerImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/form/FormContainerImpl.java
@@ -293,4 +293,11 @@ public class FormContainerImpl extends AbstractContainerImpl implements FormCont
         return StringUtils.EMPTY;
     }
 
+    @JsonIgnore
+    @Override
+    public String getName() {
+        // explicitly setting null, since form container does not have name property, but other containers like panel have a name
+        return null;
+    }
+
 }
