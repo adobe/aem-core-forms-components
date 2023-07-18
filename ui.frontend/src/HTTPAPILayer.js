2ce9b3f0c314d4812056651aca6f0f3442bd60cd
Arun Kumar Attri <aattri@adobe.com>
2023-07-18 12:06:33 +0530

    FORMS-9699: backport custom function support

--- ui.frontend/src/HTTPAPILayer.js
+++ ui.frontend/src/HTTPAPILayer.js
@@ -84,4 +84,7 @@ export default class HTTPAPILayer {
             xhr.send();
         });
     }
+    static async getCustomFunctionConfig(formId) {
+        return await this.getJson(Constants.API_PATH_PREFIX + "/customfunctions/" + formId);
+    }
 }
