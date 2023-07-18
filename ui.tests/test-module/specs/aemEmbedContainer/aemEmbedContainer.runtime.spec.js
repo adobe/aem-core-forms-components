b860ebc0a12cd412c092be2f124162e882e1522a
Deep Prakash Dewanji <deepprakash345@gmail.com>
2023-07-18 13:25:14 +0530

    Revert "[FORMS-9806] changed query from jquery to document.queryselector....."

--- ui.tests/test-module/specs/aemEmbedContainer/aemEmbedContainer.runtime.spec.js
+++ ui.tests/test-module/specs/aemEmbedContainer/aemEmbedContainer.runtime.spec.js
@@ -49,20 +49,4 @@ describe("Sites with Aem Embed Container", () => {
             cy.get('.cmp-adaptiveform-container').find('.cmp-adaptiveform-textinput__widget').should('have.length', 8);
         })
     })
-
-    context('aem embed container in iframe mode with custom height ', function () {
-
-        const pagePath = "/content/core-components-examples/library/adaptive-form/aemembedcontainerwithcustomheight.html?wcmmode=disabled";
-
-        beforeEach(function () {
-            // this is done since cypress session results in 403 sometimes
-            cy.openPage(pagePath);
-
-        })
-
-        it("test for embed container height property working in iFrameMode", () => {
-            cy.get('.cmp-aemform__iframecontent').should('have.css', 'height', '1000px');
-        })
-
-    })
 })
