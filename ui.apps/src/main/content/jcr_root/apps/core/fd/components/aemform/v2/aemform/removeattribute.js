f8ca25f34f417b912071c6402c7559c5cf6db9cb
demaurya <demaurya@adobe.com>
2023-07-21 12:34:17 +0530

    Fix core component rendition in sites

--- ui.apps/src/main/content/jcr_root/apps/core/fd/components/aemform/v2/aemform/removeattribute.js
+++ ui.apps/src/main/content/jcr_root/apps/core/fd/components/aemform/v2/aemform/removeattribute.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2022 Adobe
+ *  Copyright 2023 Adobe
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,9 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-/**
- * using property wcmmode and theme configured in af in aem.
- */
+
 use(function () {
     var property;
     for (property in this) {
