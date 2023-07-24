128520447ac62dd1008adacce6c28a77951be8ec
Ravi <ravisanker1999@gmail.com>
2023-07-24 16:45:33 +0530

    Computing median lighthouse score to decrease variablility

--- .circleci/ci/lighthouse.js
+++ .circleci/ci/lighthouse.js
@@ -23,8 +23,7 @@ const checkLightHouse = async () => {
     const aemUsername = ci.sh('mvn --file ui.tests help:evaluate -Dexpression=AEM_AUTHOR_USERNAME -q -DforceStdout', true);
     const aemPassword = ci.sh('mvn --file ui.tests help:evaluate -Dexpression=AEM_AUTHOR_PASSWORD -q -DforceStdout', true);
 
-    const lighthouse = await import('lighthouse')
-    const LHDesktopConfig = await import('lighthouse/core/config/desktop-config.js')
+    const {default: LHDesktopConfig} = await import('lighthouse/core/config/desktop-config.js')
     const chromeLauncher = await import('chrome-launcher')
     const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
     const options = {
@@ -41,19 +40,19 @@ const checkLightHouse = async () => {
     };
     const lighthouseConfig = JSON.parse(fs.readFileSync('/home/circleci/build/.circleci/ci/lighthouseConfig.json'))
 
-    const desktopRunnerResult = await lighthouse.default(lighthouseConfig.urls[0], options, LHDesktopConfig.default);// `.report` is the HTML report as a string
-    const desktopReportHtml = desktopRunnerResult.report;
-    console.log('Lighthouse Report for desktop generated', desktopRunnerResult.lhr.finalDisplayedUrl);
-    console.log(getCommentText(desktopRunnerResult.lhr.categories, 'desktop'))
+    const desktopResult = await computeMedianScores(lighthouseConfig.urls[0], options,  LHDesktopConfig) // Running lighthouse for desktop
+    console.log('Lighthouse Report for desktop generated', desktopResult.lhr.finalDisplayedUrl);
+    console.log(getCommentText(desktopResult.lhr.categories, 'desktop'))
+    const desktopReportHtml = desktopResult.report;
 
-    const defaultRunnerResult = await lighthouse.default(lighthouseConfig.urls[0], options); // by default lighthouse scores are computed for mobile device
-    const mobileReportHtml = defaultRunnerResult.report;
-    console.log('Lighthouse Report for mobile generated', defaultRunnerResult.lhr.finalDisplayedUrl);
-    console.log(getCommentText(defaultRunnerResult.lhr.categories, 'mobile'))
+    const mobileResult = await computeMedianScores(lighthouseConfig.urls[0], options) // by default lighthouse scores are computed for mobile device
+    console.log('Lighthouse Report for desktop generated', mobileResult.lhr.finalDisplayedUrl);
+    console.log(getCommentText(mobileResult.lhr.categories, 'mobile'))
+    const mobileReportHtml = mobileResult.report;
 
     if(process.env.AEM === "addon"){ // posting lighthouse scores only in case of AEM is addon
-        await ci.postCommentToGitHubFromCI(getCommentText(desktopRunnerResult.lhr.categories, 'desktop'))
-        await ci.postCommentToGitHubFromCI(getCommentText(defaultRunnerResult.lhr.categories, 'mobile'))
+        await ci.postCommentToGitHubFromCI(getCommentText(desktopResult.lhr.categories, 'desktop'))
+        await ci.postCommentToGitHubFromCI(getCommentText(mobileResult.lhr.categories, 'mobile'))
     }
     ci.sh('mkdir artifacts');
     ci.dir("artifacts", () => {
@@ -62,7 +61,7 @@ const checkLightHouse = async () => {
         });
 
 
-    const thresholdResults = checkThresholds(defaultRunnerResult.lhr.categories, lighthouseConfig) // using mobile lighthouse scores as these are usually lower
+    const thresholdResults = checkThresholds(mobileResult.lhr.categories, lighthouseConfig) // using mobile lighthouse scores as these are usually lower
     console.log("thresholdResults -->>>> ", thresholdResults)
     if(!thresholdResults.isThresholdPass && process.env.AEM === "addon"){
         console.log("Error: Lighthouse score for aem-core-forms-components, below the thresholds")
@@ -70,9 +69,27 @@ const checkLightHouse = async () => {
         process.exit(1);
     }
     else if(thresholdResults.updateLighthouseConfig && ['master', 'dev', 'release/650'].includes(process.env.CIRCLE_BRANCH) && process.env.AEM === "addon"){ // only execute if branch name is 'master'
-        writeObjLighthouseConfig(defaultRunnerResult.lhr.categories, lighthouseConfig)
+        writeObjLighthouseConfig(mobileResult.lhr.categories, lighthouseConfig)
     }
     await chrome.kill();
+}
+
+const computeMedianScores = async (url, options, config) => {
+    const {default:lighthouse} = await import('lighthouse');
+    const {computeMedianRun} = await import('lighthouse/core/lib/median-run.js');
+    const lhrs = [], results = [];
+    for(let i=0; i<5; i++) {
+        try {
+            const result = await lighthouse(url, options, config);
+            results.push(result);
+            lhrs.push(result.lhr);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    const median = computeMedianRun(lhrs);
+    const result = results[lhrs.indexOf(median)]
+    return result;
 }
 
 const getCommentText = (resultCategories, preset) => {
