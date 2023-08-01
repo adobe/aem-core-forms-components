76ba50e9cfa664e6b657f4b13b1fe0b8a055cc7e
Gulshan Mishra <gulshanm@adobe.com>
2023-08-01 15:41:54 +0530

    FORMS-9473: check visibility inside .then

--- ui.tests/test-module/libs/support/commands.js
+++ ui.tests/test-module/libs/support/commands.js
@@ -223,9 +223,11 @@ Cypress.Commands.add("openEditableToolbar", (selector) => {
                 if ($body.find(path).length === 0) {
                     //evaluates as true if toolbar doesnt exists at all
                     //you get here only if toolbar is visible
-                    cy.get(selector).click({force: true}); // end user does not face this but due to cypress checks, we need to add force true here
+                    cy.get(selector).click({force: true}).then(() => {
+                        cy.get(path).should('be.visible');
+                    }) // end user does not face this but due to cypress checks, we need to add force true here
                     // sometimes the above line results in this error, `<div.cq-Overlay.cq-Overlay--component.cq-draggable.cq-droptarget.is-resizable>` is not visible because its parent `<div.cq-Overlay.cq-Overlay--component.cq-Overlay--container>` has CSS property: `display: none`
-                    cy.get(path).should('be.visible');
+
                 } else {
                     cy.get(path).then($header => {
                         if (!$header.is(':visible')) {
