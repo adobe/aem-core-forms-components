6ce3de961d9b65991e651c6747334d3250fa80aa
Rishi Mehta <rismehta@adobe.com>
2023-08-22 16:40:57 +0530

    Fixing test

--- ui.tests/test-module/specs/recaptcha/recaptcha.runtime.spec.js
+++ ui.tests/test-module/specs/recaptcha/recaptcha.runtime.spec.js
@@ -21,64 +21,79 @@ describe("Form Runtime with Recaptcha Input", () => {
     const IS = "adaptiveFormRecaptcha"
     const selectors = {
         recaptcha : `[data-cmp-is="${IS}"]`
-    }
+    };
 
     let formContainer = null
 
-    beforeEach(() => {
-        cy.previewForm(pagePath).then((p) => {
-            formContainer = p;
+    let toggle_array = [];
+
+    before(() => {
+        cy.fetchFeatureToggles().then((response) => {
+            if (response.status === 200) {
+                toggle_array = response.body.enabled;
+            }
         });
     });
 
-    const checkHTML = (id, state) => {
-        const visible = state.visible;
-        const passVisibleCheck = `${visible === true ? "" : "not."}be.visible`;
-        const passDisabledAttributeCheck = `${state.enabled === false || state.readOnly === true ? "" : "not."}have.attr`;
-        const value = state.value
-        cy.get(`#${id}`)
-            .should(passVisibleCheck)
-            .invoke('attr', 'data-cmp-visible')
-            .should('eq', visible.toString());
-        cy.get(`#${id}`)
-            .invoke('attr', 'data-cmp-enabled')
-            .should('eq', state.enabled.toString());
-        return cy.get(`#${id}`).within((root) => {
-            cy.get('*').should(passVisibleCheck)
-        })
-    }
-
-    it(" should get model and view initialized properly ", () => {
-        expect(formContainer, "formcontainer is initialized").to.not.be.null;
-        expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
-        Object.entries(formContainer._fields).forEach(([id, field]) => {
-            expect(field.getId()).to.equal(id)
-            expect(formContainer._model.getElement(id), `model and view are in sync`).to.equal(field.getModel())
+    if (toggle_array.includes(FT_CLOUD_CONFIG_PROVIDER)) {
+        beforeEach(() => {
+            cy.previewForm(pagePath).then((p) => {
+                formContainer = p;
+            });
         });
-    })
 
-    it(" model's changes are reflected in the html ", () => {
-        const [id, fieldView] = Object.entries(formContainer._fields)[0]
-        const model = formContainer._model.getElement(id)
-        cy.get('#' + id + ' .cmp-adaptiveform-recaptcha__widget > div.g-recaptcha').should('exist');
+        const checkHTML = (id, state) => {
+            const visible = state.visible;
+            const passVisibleCheck = `${visible === true ? "" : "not."}be.visible`;
+            const passDisabledAttributeCheck = `${state.enabled === false || state.readOnly === true ? "" : "not."}have.attr`;
+            const value = state.value
+            cy.get(`#${id}`)
+                .should(passVisibleCheck)
+                .invoke('attr', 'data-cmp-visible')
+                .should('eq', visible.toString());
+            cy.get(`#${id}`)
+                .invoke('attr', 'data-cmp-enabled')
+                .should('eq', state.enabled.toString());
+            return cy.get(`#${id}`).within((root) => {
+                cy.get('*').should(passVisibleCheck)
+            })
+        }
 
-        checkHTML(model.id, model.getState()).then(() => {
-            model.visible = false
-            return checkHTML(model.id, model.getState())
-        }).then(() => {
-            model.enable = false
-            return checkHTML(model.id, model.getState())
+        it(" should get model and view initialized properly ", () => {
+            expect(formContainer, "formcontainer is initialized").to.not.be.null;
+            expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
+            Object.entries(formContainer._fields).forEach(([id, field]) => {
+                expect(field.getId()).to.equal(id)
+                expect(formContainer._model.getElement(id), `model and view are in sync`).to.equal(field.getModel())
+            });
         })
-    });
 
-    it(" html changes are reflected in model ", () => {
-        const [id, fieldView] = Object.entries(formContainer._fields)[0]
-        const model = formContainer._model.getElement(id)
-        cy.log(model.getState().value)
-        cy.get(`#${id}`).click().then(x => {
+        it(" model's changes are reflected in the html ", () => {
+            const [id, fieldView] = Object.entries(formContainer._fields)[0]
+            const model = formContainer._model.getElement(id)
+            cy.get('#' + id + ' .cmp-adaptiveform-recaptcha__widget > div.g-recaptcha').should('exist');
+
+            checkHTML(model.id, model.getState()).then(() => {
+                model.visible = false
+                return checkHTML(model.id, model.getState())
+            }).then(() => {
+                model.enable = false
+                return checkHTML(model.id, model.getState())
+            })
+        });
+
+        it(" html changes are reflected in model ", () => {
+            const [id, fieldView] = Object.entries(formContainer._fields)[0]
+            const model = formContainer._model.getElement(id)
             cy.log(model.getState().value)
-            expect(model.getState().value).to.not.be.null
-        })
-    });
-
+            cy.get(`#${id}`).click().then(x => {
+                cy.log(model.getState().value)
+                expect(model.getState().value).to.not.be.null
+            })
+        });
+    } else {
+        it('Feature toggle of captcha is disabled, skipping tests', () => {
+            cy.log('Tests were skipped because the feature toggle of captcha is disabled');
+        });
+    }
 })
