79bf745c3584ef11267781eaf01e0ca44819bf4d
sakarora <sakarora@adobe.com>
2023-09-19 16:51:38 +0530

    Adding the classic and classic latest back to the config.yml for 650.

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -24,7 +24,8 @@ console.log(config);
 const qpPath = '/home/circleci/cq';
 const buildPath = '/home/circleci/build';
 const { TYPE, BROWSER, AEM, PRERELEASE, FT } = process.env;
-const isLatestAddon = AEM === 'addon-latest';
+const classicFormAddonVersion = 'LATEST';
+const classicFormReleasedAddonVersion = '6.0.1016';
 
 try {
     ci.stage("Integration Tests");
@@ -35,6 +36,15 @@ try {
 
     let extras = ``, preleaseOpts = ``;
     if (AEM === 'classic') {
+        // Download latest add-on release from artifactory
+        ci.sh(`mvn -s ${buildPath}/.circleci/settings.xml com.googlecode.maven-download-plugin:download-maven-plugin:1.6.3:artifact -Partifactory-cloud -DgroupId=com.adobe.aemds -DartifactId=adobe-aemfd-linux-pkg -Dversion=${classicFormReleasedAddonVersion} -Dtype=zip -DoutputDirectory=${buildPath} -DoutputFileName=forms-linux-addon.far`);
+        extras += ` --install-file ${buildPath}/forms-linux-addon.far`;
+        // The core components are already installed in the Cloud SDK
+        extras += ` --bundle com.adobe.cq:core.wcm.components.all:${wcmVersion}:zip`;
+    } else if (AEM === 'classic-latest') {
+        // Download latest add-on release from artifactory
+        ci.sh(`mvn -s ${buildPath}/.circleci/settings.xml com.googlecode.maven-download-plugin:download-maven-plugin:1.6.3:artifact -Partifactory-cloud -DgroupId=com.adobe.aemds -DartifactId=adobe-aemfd-linux-pkg -Dversion=${classicFormAddonVersion} -Dtype=zip -DoutputDirectory=${buildPath} -DoutputFileName=forms-linux-addon.far`);
+        extras += ` --install-file ${buildPath}/forms-linux-addon.far`;
         // The core components are already installed in the Cloud SDK
         extras += ` --bundle com.adobe.cq:core.wcm.components.all:${wcmVersion}:zip`;
     } else if (AEM === 'addon') {
@@ -74,6 +84,7 @@ try {
             ${extras} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-apps'] /*, isLatestAddon ? true : false */)} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-af-apps'] /*, isLatestAddon ? true : false */)} \
+            ${ci.addQpFileDependency(config.modules['core-forms-components-core'])} \\
             ${ci.addQpFileDependency(config.modules['core-forms-components-af-core'])} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-examples-apps'])} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-examples-content'])} \
@@ -84,6 +95,14 @@ try {
             ${ci.addQpFileDependency(config.modules['core-forms-components-it-tests-content'])} \
             --vm-options \\\"-Xmx4096m -XX:MaxPermSize=1024m -Djava.awt.headless=true -javaagent:${process.env.JACOCO_AGENT}=destfile=crx-quickstart/jacoco-it.exec\\\" \
             ${preleaseOpts}`);
+
+        if (AEM === 'classic' || AEM === 'classic-latest') {
+            // add a sleep for 5 mins, add-on takes times to come up
+            ci.sh(`sleep 5m`);
+            // restart the AEM insatnce
+            ci.sh(`./qp.sh stop --id author`);
+            ci.sh(`./qp.sh start --id author`);
+        }
 });
 
     // Run integration tests
