9904a0efa4769d13a3a45fc2d222d84d10e71b82
Gulshan Mishra <egmox2@gmail.com>
2023-10-11 14:04:19 +0530

    Forms 7896 switch component2 (#878)
    * Added Switch component and resolved PR comments
    
    * Resolved PR comments
    
    Abstracted nitialize common logic for switch and checkbox
    Added test cases
    Fixed bug with enableUncheckedValue sync after save
    updated js docs and java docs
    fixed bem
    updated abstraction logic for switch and checkbox
    
    * skipped temporarily tests due to dialog bugs
    
    * Refactored code. ternery operator instead of if else
    fixed bem
    fixed flaky tests
    bumped package-info version
    
    * FORMS-11284 fixing enum name display on hide (#911)
    
    * FORMS-11284 fixing enum name display on hide
    
    * FORMS-11284 adding test case
    
    Co-authored-by: Shivam Agarwal <shivama@adobe.com>
    
    ---------
    
    Co-authored-by: Gulshan Mishra <gulshanm@adobe.com>
    Co-authored-by: Shivam Agarwal <47965724+im-shiv@users.noreply.github.com>
    Co-authored-by: Shivam Agarwal <shivama@adobe.com>

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/replacehook.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/replacehook.js
@@ -19,6 +19,7 @@
         BINARY: 'binary',
         TEXT: 'text',
         SELECT: 'select',
+        CHECKBOX: 'checkBox',
         LIST: 'list',
         DATE: 'date',
         NON_INPUT: 'nonInputReadOnly',
@@ -27,6 +28,7 @@
     const typeMap = {
         'button': fieldTypes.NON_INPUT,
         'checkbox-group': fieldTypes.SELECT,
+        'checkbox': fieldTypes.CHECKBOX,
         'date-input': fieldTypes.TEXT,
         'drop-down': fieldTypes.SELECT,
         'email': fieldTypes.TEXT,
@@ -44,8 +46,8 @@
     const preservedProperties = ['id', 'description', 'enabled', 'jcr:created', 'jcr:title', 'name',
         'placeholder', 'readOnly', 'required', 'tooltip', 'visible', 'enum', 'enumNames'];
 
-    const cannotBeReplacedWith = ['file-input', 'checkbox'],
-        irreplaceable = ['file-input', 'checkbox'],
+    const cannotBeReplacedWith = ['file-input'],
+        irreplaceable = ['file-input'],
         editableJsonPath = '.model.json',
         componentJsonPath = '.json';
 
