3c724a24cc01f4a9eddeb7090435f2516ca1d7b8
sakarora <sakarora@adobe.com>
2023-10-27 15:18:34 +0530

    Adding another aem restart.

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -104,6 +104,11 @@ try {
             ci.sh(`./qp.sh start --id author`);
             // add a sleep for 5 mins, add-on takes times to come up
             ci.sh(`sleep 5m`);
+            // restart the AEM insatnce
+            ci.sh(`./qp.sh stop --id author`);
+            ci.sh(`./qp.sh start --id author`);
+            // add a sleep for 5 mins, add-on takes times to come up
+            ci.sh(`sleep 5m`);
         }
 });
 
