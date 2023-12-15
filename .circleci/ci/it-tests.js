96ddea961f3ec1f0569da047399050b87e30aa36
Rishi Mehta <rismehta@adobe.com>
2023-12-15 16:17:21 +0530

    Adding vm options to java environment

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -84,7 +84,7 @@ try {
             ${ci.addQpFileDependency(config.modules['core-forms-components-it-tests-core'])} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-it-tests-apps'])} \
             ${ci.addQpFileDependency(config.modules['core-forms-components-it-tests-content'])} \
-            --vm-options \\\"-Xmx4096m -XX:MaxPermSize=1024m -Djava.awt.headless=true -javaagent:${process.env.JACOCO_AGENT}=destfile=crx-quickstart/jacoco-it.exec\\\" \
+            --vm-options \\\"-Xmx4096m -XX:MaxPermSize=1024m -Djava.awt.headless=true -javaagent:${process.env.JACOCO_AGENT}=destfile=crx-quickstart/jacoco-it.exec -DFORMS_DOCUMENT_SERVICE=forms-docservice-java:8080\\\" \
             ${preleaseOpts}`);
 });
 
