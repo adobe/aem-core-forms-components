3cda33b43931bc4d3b63b2c794ca3e9da3a0212b
Rishi Mehta <rismehta@adobe.com>
2024-01-18 12:59:52 +0530

    Fixing cp test issues

--- ui.tests/test-module/libs/support/commands.js
+++ ui.tests/test-module/libs/support/commands.js
@@ -144,7 +144,7 @@ Cypress.Commands.add("openAFv2TemplateEditor", () => {
 // Cypress command to open template editor
 Cypress.Commands.add("openTemplateEditor", (templatePath) => {
     const contextPath = Cypress.env('crx.contextPath') ? Cypress.env('crx.contextPath') : "";
-    const path = `${contextPath}/editor.html${templatePath}`;
+    const path = contextPath ? `${contextPath}/editor.html${templatePath}` : `editor.html${templatePath}`;
     cy.enableOrDisableTutorials(false);
     cy.visit(path, {'failOnStatusCode': false}).then(waitForEditorToInitialize);
     preventClickJacking();
@@ -229,7 +229,7 @@ Cypress.Commands.add("getFormJson", (pagePath) => {
 // Cypress command to open template editor
 Cypress.Commands.add("openTemplateEditor", (templatePath) => {
     const contextPath = Cypress.env('crx.contextPath') ? Cypress.env('crx.contextPath') : "";
-    const path = `${contextPath}/editor.html${templatePath}`;
+    const path = contextPath ? `${contextPath}/editor.html${templatePath}` : `editor.html${templatePath}`;
     cy.enableOrDisableTutorials(false);
     cy.visit(path, {'failOnStatusCode': false}).then(waitForEditorToInitialize);
     preventClickJacking();
@@ -248,15 +248,17 @@ Cypress.Commands.add("openAuthoring", (pagePath) => {
 
 // Cypress command to open authoring page
 Cypress.Commands.add("openPage", (pagePath, options = {}) => {
+    const contextPath = Cypress.env('crx.contextPath') ? Cypress.env('crx.contextPath') : "";
+    let path = contextPath ? `${contextPath}/${pagePath}` : pagePath;
     if (!options.noLogin) {
-        // getting status 403 intermittently, just ignore it
-        const baseUrl = Cypress.env('crx.contextPath') ? Cypress.env('crx.contextPath') : "";
+    // getting status 403 intermittently, just ignore it
+        const baseUrl = contextPath;
         cy.visit(baseUrl, {'failOnStatusCode': false});
         cy.login(baseUrl, () => {
-            cy.openPage(pagePath, options);
+            cy.openPage(path, options);
         });
     }
-    cy.visit(pagePath, options);
+    cy.visit(path, options);
 });
 
 // cypress command to select layer in authoring
@@ -453,7 +455,7 @@ Cypress.Commands.add("getFromDefinitionUsingOpenAPI", (formPath, offset = 0, lim
 
 Cypress.Commands.add("previewForm", (formPath, options = {}) => {
     const contextPath = Cypress.env('crx.contextPath') ? Cypress.env('crx.contextPath') : "";
-    let pagePath = `${contextPath}/${formPath}?wcmmode=disabled`;
+    let pagePath = contextPath ? `${contextPath}/${formPath}?wcmmode=disabled` : `${formPath}?wcmmode=disabled`;
     if (options?.params) {
         options.params.forEach((param) => pagePath += `&${param}`)
         delete options.params
