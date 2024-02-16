78b8937fa37f35c67fa61cf5a2d36046e99cc2dc
s1mahanty <110378842+s1mahanty@users.noreply.github.com>
2024-02-16 12:24:15 +0530

    FORMS-12577: Adding title in Aem Embed for accessibility (#1068)
    * FORMS-12577: Adding title in aem embed for accessibility
    
    * FORMS-12577: Review comments incorporated
    
    * FORMS-12577: Review comments incorporated
    
    * Move strings to constants

--- bundles/core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/aemform/AEMFormImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v2/aemform/AEMFormImpl.java
@@ -17,17 +17,26 @@ package com.adobe.cq.forms.core.components.internal.models.v2.aemform;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.caconfig.ConfigurationBuilder;
 import org.apache.sling.caconfig.ConfigurationResolver;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.jetbrains.annotations.Nullable;
 
+import com.adobe.aemds.guide.utils.GuideUtils;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.forms.core.components.internal.models.v2.HtmlPageItemImpl;
@@ -35,6 +44,8 @@ import com.adobe.cq.forms.core.components.models.aemform.AEMForm;
 import com.adobe.cq.wcm.core.components.config.HtmlPageItemConfig;
 import com.adobe.cq.wcm.core.components.config.HtmlPageItemsConfig;
 import com.adobe.cq.wcm.core.components.models.HtmlPageItem;
+import com.day.cq.i18n.I18n;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Model(
     adaptables = SlingHttpServletRequest.class,
@@ -44,11 +55,26 @@ import com.adobe.cq.wcm.core.components.models.HtmlPageItem;
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class AEMFormImpl extends com.adobe.cq.forms.core.components.internal.models.v1.aemform.AEMFormImpl implements AEMForm {
     public static final String RESOURCE_TYPE = "core/fd/components/aemform/v2/aemform";
+    private static final String FORM_TITLE_PROPERTY_NAME = "title";
 
     @OSGiService
     private ConfigurationResolver configurationResolver;
 
     private List<HtmlPageItem> htmlPageItems;
+
+    @ValueMapValue(name = "jcr:title", injectionStrategy = InjectionStrategy.OPTIONAL)
+    private String jcrTitle;
+
+    private I18n i18n;
+
+    @PostConstruct
+    protected void init() {
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        String localeString = resourceResolver.map(getLocale());
+        Locale locale = Locale.forLanguageTag(localeString);
+        ResourceBundle resourceBundle = request.getResourceBundle(locale);
+        this.i18n = new I18n(resourceBundle);
+    }
 
     @Override
     public @Nullable List<HtmlPageItem> getHtmlPageItems() {
@@ -66,4 +92,25 @@ public class AEMFormImpl extends com.adobe.cq.forms.core.components.internal.mod
         }
         return htmlPageItems;
     }
+
+    @JsonIgnore
+    public String getTitle() {
+        String formTitle = "";
+        if (jcrTitle != null && !jcrTitle.isEmpty()) {
+            formTitle = jcrTitle;
+        } else {
+            // Fallback to form title if jcr:title is not available
+            Resource formResource = request.getResourceResolver().getResource(getFormPagePath());
+            if (formResource != null) {
+                ValueMap formProperties = formResource.getValueMap();
+                formTitle = formProperties.get(FORM_TITLE_PROPERTY_NAME, "");
+                if (!StringUtils.isBlank(formTitle) && i18n != null) {
+                    // Translate the formTitle
+                    formTitle = GuideUtils.translateOrReturnOriginal(formTitle, FORM_TITLE_PROPERTY_NAME, i18n, formResource.getValueMap());
+                }
+            }
+        }
+        return formTitle;
+    }
+
 }
