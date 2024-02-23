131176313b4fb9d23397503a4fdacdb15a586a13
Barshat Rai <barshatrai99@gmail.com>
2024-02-23 13:10:17 +0530

    ignoring shellMenuButton console error

--- ui.tests/test-module/libs/support/index.js
+++ ui.tests/test-module/libs/support/index.js
@@ -100,6 +100,10 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     }
 
+    if (err.message.includes("shellMenuButton")) {
+        return false;
+    }
+
     // we still want to ensure there are no other unexpected
     // errors, so we let them fail the test
     return true;
