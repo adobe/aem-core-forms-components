cc93e89ae61276b0a189c184c16b2e9c0e8be48c
marikannanm <mariappa@adobe.com>
2024-03-11 16:30:55 +0530

    imagechoice-component - removed notnull

--- bundles/af-core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/form/ImageChoiceImplTest.java
+++ bundles/af-core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/form/ImageChoiceImplTest.java
@@ -364,6 +364,7 @@ public class ImageChoiceImplTest {
         ImageChoice imageChoice = getImageChoiceUnderTest(PATH_IMAGECHOICE_CUSTOMIZED);
         assertEquals("single", ((ImageChoiceImpl) imageChoice).getSelectionType());
     }
+
     @Test
     void testGetSelectionTypeWithMultiple() {
         ImageChoice imageChoice = getImageChoiceUnderTest(PATH_IMAGECHOICE);
