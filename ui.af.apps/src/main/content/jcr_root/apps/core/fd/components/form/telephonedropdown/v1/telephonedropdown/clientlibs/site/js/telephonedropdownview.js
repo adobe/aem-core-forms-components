5d1051cad3ece9858c718d4b43207eec6ac6f4c7
Shivangi Mittal <shivangim@Shivangis-MacBook-Pro.local>
2024-03-12 00:14:19 +0530

    Added set model parser for phoennumbera nd countrycode

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/telephonedropdown/v1/telephonedropdown/clientlibs/site/js/telephonedropdownview.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/telephonedropdown/v1/telephonedropdown/clientlibs/site/js/telephonedropdownview.js
@@ -74,39 +74,77 @@
             return this.element.querySelector(TelephoneDropdown.selectors.widget)
         }
 
+        updateValue(value) {
+            if (this.widgetObject == null && (this._model._jsonModel.editFormat || this._model._jsonModel.displayFormat || FormView.Utils.isUserAgent('safari'))) {
+                this.initializeWidget();
+            }
+            if (this.widgetObject) {
+                this.widgetObject.setValue(value);
+                super.updateEmptyStatus();
+            } else {
+                super.updateValue(value);
+            }
+        }
+
         setModel(model) {
             super.setModel(model);
             if (this.widget.value !== '') {
-                this._model.value = this.widget.value;
+                this._model.value = this.getCountryCodeSelect().value + this.widget.value;
             }
             this.widget.addEventListener('blur', (e) => {
-                this._model.value = e.target.value;
                 this.setInactive();
             });
             this.widget.addEventListener('focus', (e) => {
                 this.setActive();
             });
 
+            var countryCodes = [];
+
+            function populateCountryCodes() {
+                var selectElement = document.querySelector(TelephoneDropdown.selectors.countryCode);
+                var optionElements = selectElement.querySelectorAll('option');
+                for (var i = 0; i < optionElements.length; i++) {
+                    var countryCode = optionElements[i].value;
+                    countryCodes.push(countryCode);
+                }
+            }
+
+            if (document.readyState === 'loading') {  // Loading hasn't finished yet
+                document.addEventListener('DOMContentLoaded', populateCountryCodes);
+            } else {  // `DOMContentLoaded` has already fired
+                populateCountryCodes();
+            }
+
+            function parsePhoneNumber(phoneNumber) {
+                // Find the country code and the rest of the phone number
+                let countryCode = '';
+                let number = phoneNumber;
+                for(var i = 0; i < countryCodes.length; i++){
+                    let code = countryCodes[i];
+                    if (phoneNumber.startsWith(code)) {
+                        countryCode = code;
+                        number = phoneNumber.slice(code.length);
+                        break;
+                    }
+                }
+                return {countryCode, number };
+            }
+
             this.getPhoneNumber().addEventListener('change', (e) => {
                 let countryCode = this.getCountryCodeSelect().value;
                 this._model.value = countryCode + e.target.value;
+                e.target.value = parsePhoneNumber(e.target.value).number;
+                this.getCountryCodeSelect().value = countryCode;
             });
 
             this.getCountryCodeSelect().addEventListener('change', (e) => {
                 let phoneNumber = this.getPhoneNumber().value;
                 this._model.value = e.target.value + phoneNumber;
+                this.getPhoneNumber().value = parsePhoneNumber(this._model.value).number;
+                e.target.value = parsePhoneNumber(this._model.value).countryCode;
             });
 
-            // this.getPhoneNumber().addEventListener('change', (e) => {
-            //     let countryCode = this.getCountryCodeSelect().value;
-            //     this._model.value = countryCode + e.target.value;
-            // });
-            //
-            // this.getCountryCodeSelect().addEventListener('change', (e) => {
-            //     local = e.target.value;
-            //     // let phoneNumber = this.getPhoneNumber().value;
-            //     // this._model.value = e.target.value + phoneNumber;
-            // });
+
         }
     }
 
