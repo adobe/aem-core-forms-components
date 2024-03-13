344bbe6e911d51b4c3407552e234d59ba7e152a9
Shivangi Mittal <shivangim@Shivangis-MacBook-Pro.local>
2024-03-13 22:08:15 +0530

    FORMS-12203: Added test for wizard component string language

--- ui.tests/test-module/specs/wizard/wizard.authoring.spec.js
+++ ui.tests/test-module/specs/wizard/wizard.authoring.spec.js
@@ -46,6 +46,14 @@ describe('Page - Authoring', function () {
         cy.get('body').click(0, 0);
     }
 
+    const changeLanguage = function (str) {
+        cy.visit('http://localhost:4502/aem/forms.html/content/dam/formsanddocuments');
+        cy.get('[data-foundation-toggleable-control-src="/mnt/overlay/granite/ui/content/shell/userproperties.html"]').click();
+        cy.get('[data-foundation-toggleable-control-src="/mnt/overlay/granite/ui/content/shell/userpreferences.html"]').click();
+        cy.get('[name="language"].coral-Form-field._coral-Dropdown').first().click();
+        cy.get(`coral-selectlist-item[value=${str}]`).click({force: true});
+        cy.get('._coral-Button._coral-Button--cta[trackingelement="accept"]').click();
+    }
 
     context('Open Forms Editor', function () {
         const pagePath = "/content/forms/af/core-components-it/blank",
@@ -155,4 +163,40 @@ describe('Page - Authoring', function () {
         });
 
     });
+
+    context('Test Wizard Component String Language', function (){
+        beforeEach(function () {
+            // this is done since cypress session results in 403 sometimes
+            cy.visit('http://localhost:4502/aem/start.html');
+            cy.get('#username').type('admin');
+            cy.get('#password').type('admin');
+            cy.get('#submit-button').click();
+            cy.on('uncaught:exception', () => {
+                return false
+            });
+        });
+        it('Wizard String language test', function ()  {
+            changeLanguage("de");
+            cy.visit('http://localhost:4502/libs/fd/fm/gui/content/forms/af/create.html').then(() => {
+                // Your tests here
+                cy.wait(30000);
+                cy.get('[data-item-id="/conf/core-components-examples/settings/wcm/templates/af-blank-v2"]').click();
+                cy.get('.spectrum-Button_e2d99e.spectrum-Button--cta_e2d99e').click();
+                cy.get('input[name="submitDialogTitle"]').type('language-test');
+                cy.get('.spectrum-Button_e2d99e.spectrum-Button--cta_e2d99e.spectrum-ButtonGroup-Button_b10c2f').click();
+            });
+
+            changeLanguage("en");
+            cy.visit('http://localhost:4502/editor.html/content/forms/af/language-test.html');
+            const dataPath = "/content/forms/af/language-test/jcr:content/guideContainer/*",
+                responsiveGridDropZoneSelector = sitesSelectors.overlays.overlay.component + "[data-path='" + dataPath + "']";
+            cy.selectLayer("Edit");
+            cy.insertComponent(responsiveGridDropZoneSelector, "Adaptive Form Wizard Layout", afConstants.components.forms.resourceType.wizard);
+            cy.get('body').click(0, 0);
+            cy.get('[data-path="/content/forms/af/language-test/jcr:content/guideContainer/wizard/*"]').invoke('attr', 'data-text').should('equal', 'Please drag Wizard components here');
+            cy.visit("http://localhost:4502/aem/formdetails.html/content/dam/formsanddocuments/language-test");
+            cy.get('.betty-ActionBar-item.formsmanager-admin-action-delete.cq-formsmanager-admin-actions-delete-activator').click();
+            cy.get('button._coral-Button._coral-Button--warning').click();
+        });
+    })
 })
