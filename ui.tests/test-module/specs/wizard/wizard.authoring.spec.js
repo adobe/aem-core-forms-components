7f4e279c4b36517dfbb77a6382eacee0b7284d31
shivangimittal791 <54174324+shivangimittal791@users.noreply.github.com>
2024-03-27 16:48:25 +0530

    Update wizard.authoring.spec.js

--- ui.tests/test-module/specs/wizard/wizard.authoring.spec.js
+++ ui.tests/test-module/specs/wizard/wizard.authoring.spec.js
@@ -37,7 +37,7 @@ describe('Page - Authoring', function () {
 
         cy.get('body').click(0, 0);
     }
-    
+
     const dropWizardInSites = function () {
         const dataPath = "/content/core-components-examples/library/adaptive-form/wizard/jcr:content/root/responsivegrid/demo/component/guideContainer/*",
             responsiveGridDropZoneSelector = sitesSelectors.overlays.overlay.component + "[data-path='" + dataPath + "']";
@@ -46,7 +46,7 @@ describe('Page - Authoring', function () {
         cy.get('body').click(0, 0);
     }
 
-    
+
     context('Open Forms Editor', function () {
         const pagePath = "/content/forms/af/core-components-it/blank",
             wizardLayoutDrop = pagePath + afConstants.FORM_EDITOR_FORM_CONTAINER_SUFFIX + "/" + afConstants.components.forms.resourceType.wizard.split("/").pop(),
@@ -153,5 +153,6 @@ describe('Page - Authoring', function () {
 
             });
         });
-        
+
     });
+})
