8f735de54dbce56fd9d9c09d14ad019a677a5766
Shrikha <smahanty@adobe.com>
2024-03-27 14:59:12 +0530

    Changes made in ButtonImpl to align with the schema

---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/ModalImpl.java
@@ -0,0 +1,2 @@
+package com.adobe.cq.forms.core.components.internal.models.v1.form;public class ModalImpl {
+}
---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/models/form/Modal.java
@@ -0,0 +1,2 @@
+package com.adobe.cq.forms.core.components.models.form;public class Modal {
+}
