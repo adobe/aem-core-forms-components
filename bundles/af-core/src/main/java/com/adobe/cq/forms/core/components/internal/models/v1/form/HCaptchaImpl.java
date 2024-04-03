743af147b30001cdc3b70d6d26fab8393db41994
Navneet Agarwal <navneeta@adobe.com>
2024-04-03 19:16:49 +0530

    adding hcaptcha and recaptcha models

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/HCaptchaImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/HCaptchaImpl.java
@@ -1,5 +1,5 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Copyright 2023 Adobe
+ ~ Copyright 2024 Adobe
  ~
  ~ Licensed under the Apache License, Version 2.0 (the "License");
  ~ you may not use this file except in compliance with the License.
@@ -37,17 +37,17 @@ import com.adobe.aemds.guide.service.GuideException;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.forms.core.components.internal.form.FormConstants;
-import com.adobe.cq.forms.core.components.models.form.ReCaptcha;
+import com.adobe.cq.forms.core.components.models.form.HCaptcha;
 import com.adobe.cq.forms.core.components.util.AbstractCaptchaImpl;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Model(
     adaptables = { SlingHttpServletRequest.class, Resource.class },
-    adapters = { ReCaptcha.class,
+    adapters = { HCaptcha.class,
         ComponentExporter.class },
-    resourceType = { FormConstants.RT_FD_FORM_RECAPTCHA_V1 })
+    resourceType = { FormConstants.RT_FD_FORM_HCAPTCHA_V1 })
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class RecaptchaImpl extends AbstractCaptchaImpl implements ReCaptcha {
+public class HCaptchaImpl extends AbstractCaptchaImpl implements HCaptcha {
 
     @Inject
     private ResourceResolver resourceResolver;
@@ -93,7 +93,7 @@ public class RecaptchaImpl extends AbstractCaptchaImpl implements ReCaptcha {
 
     @Override
     public String getProvider() {
-        return "recaptcha";
+        return "hcaptcha";
     }
 
     @JsonIgnore
