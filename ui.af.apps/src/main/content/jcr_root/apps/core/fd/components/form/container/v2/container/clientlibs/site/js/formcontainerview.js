d5f66d47a75745af6d64e352fad54536b2cf8601
Pankaj Girotra <pank@adobe.com>
2024-04-29 12:00:15 +0530

    [FORMS-11581] Enabled Draft functionality for V2

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/site/js/formcontainerview.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/site/js/formcontainerview.js
@@ -57,6 +57,21 @@
                     FormView.customFunctions.defaultSubmitErrorHandler(defaultSubmissionError, globals);
                 }
             }, "submitError");
+            this._model.subscribe((action) => {
+                let state = action.target.getState();
+                // execute the handler only if there are no rules configured on submitSuccess event.
+                if (!state.events['custom:saveSuccess'] || state.events['custom:saveSuccess'].length === 0) {
+                    console.log("Draft id = " + action?.payload?.body?.draftId);
+                    window.alert("Draft has been saved successfully");
+                }
+            }, "saveSuccess");
+            this._model.subscribe((action) => {
+                let state = action.target.getState();
+                // execute the handler only if there are no rules configured on submitSuccess event.
+                if (!state.events['custom:saveError'] || state.events['custom:saveError'].length === 0) {
+                    window.alert("Issue while saving draft");
+                }
+            }, "saveError");
         }
     }
 
