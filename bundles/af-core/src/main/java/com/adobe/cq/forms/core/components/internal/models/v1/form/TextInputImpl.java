4726a3801cdd0a84205ef26be58eb6e8081588b4
shubham-adobeaem <117734609+shubham-adobeaem@users.noreply.github.com>
2024-05-01 17:47:26 +0530

    fromatted

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/TextInputImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/TextInputImpl.java
@@ -1,27 +1,25 @@
-/*
- * Copyright 2022 Adobe
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.forms.core.components.internal.models.v1.form;
 
-import com.adobe.cq.export.json.ComponentExporter;
-import com.adobe.cq.export.json.ExporterConstants;
-import com.adobe.cq.forms.core.components.internal.form.FormConstants;
-import com.adobe.cq.forms.core.components.models.form.FieldType;
-import com.adobe.cq.forms.core.components.models.form.TextInput;
-import com.adobe.cq.forms.core.components.util.AbstractFieldImpl;
-import com.adobe.cq.forms.core.components.util.ComponentUtils;
+import java.util.Date;
+
+import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
@@ -30,32 +28,29 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import javax.annotation.Nullable;
-import javax.annotation.PostConstruct;
-import java.util.Date;
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.forms.core.components.internal.form.FormConstants;
+import com.adobe.cq.forms.core.components.models.form.FieldType;
+import com.adobe.cq.forms.core.components.models.form.TextInput;
+import com.adobe.cq.forms.core.components.util.AbstractFieldImpl;
+import com.adobe.cq.forms.core.components.util.ComponentUtils;
 
 @Model(
-        adaptables = { SlingHttpServletRequest.class, Resource.class },
-        adapters = { TextInput.class, ComponentExporter.class },
-        resourceType = {
-                FormConstants.RT_FD_FORM_TEXT_V1,
-                FormConstants.RT_FD_FORM_EMAIL_V1,
-                FormConstants.RT_FD_FORM_TELEPHONE_V1
-        }
-)
+    adaptables = { SlingHttpServletRequest.class, Resource.class },
+    adapters = { TextInput.class, ComponentExporter.class },
+    resourceType = { FormConstants.RT_FD_FORM_TEXT_V1, FormConstants.RT_FD_FORM_EMAIL_V1, FormConstants.RT_FD_FORM_TELEPHONE_V1 })
 @Exporter(
-        name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
-        extensions = ExporterConstants.SLING_MODEL_EXTENSION
-)
+    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class TextInputImpl extends AbstractFieldImpl implements TextInput {
+
+    // type number and date are implemented in sling model as per crispr specification
+    // but it is not supported in AEM dialogs
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     @Default(booleanValues = false)
     protected boolean multiLine;
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    @Default(booleanValues = false)
-    protected boolean showCharCounter;
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     @Nullable
@@ -69,9 +64,11 @@ public class TextInputImpl extends AbstractFieldImpl implements TextInput {
     @Nullable
     protected String autocomplete;
 
-    private Object exclusiveMinimumValue;
+    /** Type number specific constraints **/
+    private Object exclusiveMinimumVaue;
     private Object exclusiveMaximumValue;
 
+    /** End of Type number specific constraints **/
     @Override
     public boolean isMultiLine() {
         return multiLine;
@@ -130,7 +127,7 @@ public class TextInputImpl extends AbstractFieldImpl implements TextInput {
     @Override
     @Nullable
     public Long getExclusiveMinimum() {
-        return (Long) exclusiveMinimumValue;
+        return (Long) exclusiveMinimumVaue;
     }
 
     @Override
@@ -150,12 +147,7 @@ public class TextInputImpl extends AbstractFieldImpl implements TextInput {
 
     @Override
     public Date getExclusiveMinimumDate() {
-        return ComponentUtils.clone((Date) exclusiveMinimumValue);
-    }
-
-    @Override
-    public boolean showCharCounter() {
-        return showCharCounter;
+        return ComponentUtils.clone((Date) exclusiveMinimumVaue);
     }
 
     @Override
@@ -167,12 +159,12 @@ public class TextInputImpl extends AbstractFieldImpl implements TextInput {
     @PostConstruct
     private void initTextInput() {
         exclusiveMaximumValue = ComponentUtils.getExclusiveValue(exclusiveMaximum, maximum, null);
-        exclusiveMinimumValue = ComponentUtils.getExclusiveValue(exclusiveMinimum, minimum, null);
-
+        exclusiveMinimumVaue = ComponentUtils.getExclusiveValue(exclusiveMinimum, minimum, null);
+        // in json either, exclusiveMaximum or maximum should be present
         if (exclusiveMaximumValue != null) {
             maximum = null;
         }
-        if (exclusiveMinimumValue != null) {
+        if (exclusiveMinimumVaue != null) {
             minimum = null;
         }
     }
