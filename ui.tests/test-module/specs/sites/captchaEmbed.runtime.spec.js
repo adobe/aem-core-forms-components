aa543a3a6c884e334379d9acaa8f0b3b59efc6c2
Navneet Agarwal <53942457+navneet-ag@users.noreply.github.com>
2024-05-03 11:38:43 +0530

    FORMS-13811 | Adding rendition for hCaptcha (#1174)
    * embedding forms-sdk api
    
    * adding hcaptcha and recaptcha models
    
    * :fix hcaptcha model SlingException
    
    * :fix deleting recaptcha model
    
    * FORMS-13811 | Adding rendition for hCaptcha
    
    * FORMS-13811 | Renamed public API
    
    * FORMS-13811 | Embedding hcaptcha js
    
    * FORMS-13811 | Adding unit test for sling model
    
    * FORMS-13811 | Adding cypress test
    
    * FORMS-13811 | Adressing review comments
    
    * Adding e2e test
    
    * FORMS-13811 | fix import statements
    
    * Update pom.xml
    
    * Adding FT for running Tests
    
    * FORMS-13811 | Removing java-8 build for master
    
    * Adding FT in test
    
    * FORMS-13811 | Tests should run only with latest addon
    
    * FORMS-13811 | Tests should run only with latest addon
    
    * increasing coverage
    
    * addressing review comments
    
    ---------
    
    Co-authored-by: Navneet Agarwal <navneeta@adobe.com>

--- ui.tests/test-module/specs/sites/captchaEmbed.runtime.spec.js
+++ ui.tests/test-module/specs/sites/captchaEmbed.runtime.spec.js
@@ -15,20 +15,44 @@
  ******************************************************************************/
 describe("Captcha In Sites Runtime Test", () => {
     const pagePath = "content/forms/sites/core-components-it/site-with-captcha-afv2-form.html";
+    const hCaptchaPagePath = "content/forms/sites/core-components-it/site-with-hcaptcha-afv2-form.html";
+    const FT_HCAPTCHA = "FT_FORMS-12407";
 
     let formContainer = null;
 
-    beforeEach(() => {
-        cy.previewForm(pagePath).then(p => {
-            formContainer = p;
-        })
+    let toggle_array = [];
+
+    before(() => {
+        cy.fetchFeatureToggles().then((response) => {
+            if (response.status === 200) {
+                toggle_array = response.body.enabled;
+            }
+        });
     });
 
+
     it("captcha should render when form is embedded in site", () => {
-        expect(formContainer, "formcontainer is initialized").to.not.be.null;
-        expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
-        const [id, fieldView] = Object.entries(formContainer._fields)[0]
-        const model = formContainer._model.getElement(id)
-        cy.get('#' + id + ' .cmp-adaptiveform-recaptcha__widget > div.g-recaptcha').should('exist');
+        cy.previewForm(pagePath).then(p => {
+            formContainer = p;
+            expect(formContainer, "formcontainer is initialized").to.not.be.null;
+            expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
+            const [id, fieldView] = Object.entries(formContainer._fields)[0]
+            const model = formContainer._model.getElement(id)
+            cy.get('#' + id + ' .cmp-adaptiveform-recaptcha__widget > div.g-recaptcha').should('exist');
+        })
     })
+
+    it("hcaptcha should render when form is embedded in site", () => {
+        if (cy.af.isLatestAddon() && toggle_array.includes(FT_HCAPTCHA)) {
+            cy.previewForm(hCaptchaPagePath).then(p => {
+                formContainer = p;
+                expect(formContainer, "formcontainer is initialized").to.not.be.null;
+                expect(formContainer._model.items.length, "model and view elements match").to.equal(Object.keys(formContainer._fields).length);
+                const [id, fieldView] = Object.entries(formContainer._fields)[0]
+                const model = formContainer._model.getElement(id)
+                cy.get('#' + id + ' .cmp-adaptiveform-hcaptcha__widget > div.h-captcha').should('exist');
+            })
+        }
+    })
+
 })
