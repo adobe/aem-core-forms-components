7873513532ec1d18273b130d900223d04b13c0e2
Deep Prakash Dewanji <deepprakashdewanji@labusers-MacBook-Pro-9.local>
2024-05-03 16:27:09 +0530

    added test cases for v2 version of page component....

---
+++ ui.tests/test-module/specs/page/page.runtime.spec.js
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright 2024 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+describe("Form with Number Input", () => {
+
+    const pagePath = "content/forms/af/core-components-it/samples/page/afv2.html"
+    let formContainer = null
+
+    beforeEach(() => {
+        cy.previewForm(pagePath).then(p => {
+            formContainer = p;
+        })
+    });
+
+    it(" form should get rendered with main tag in body ", () => {
+        expect(formContainer, "formcontainer is initialized").to.not.be.null;
+        cy.get("html body main form").should("exist");
+    })
+})
