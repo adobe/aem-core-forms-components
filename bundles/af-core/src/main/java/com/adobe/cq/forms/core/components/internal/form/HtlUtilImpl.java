cf9da8116b4b17de00764692807e0643c6fc34b4
Deep Prakash Dewanji <deepprakash345@gmail.com>
2024-05-07 13:31:47 +0530

    eds semantic html without a separate selector (#1194)
    * added changes to publish semantic html of form from container[FormContainerEds].....
    
    * added test case to verify formDefinition api....
    
    * added test case to verify formDefinition api....
    
    * updated the license header of body file...
    
    * added changes to publish semantic html of form from container[FormContainerEds].....
    
    * added test case to verify formDefinition api....
    
    * added test case to verify formDefinition api....
    
    * updated the license header of body file...
    
    * created a new version of page component.....
    
    * added a Utils file to move certain logic out of FormContainer sling model
    
    * made changes to remove style elements from container proxy...
    
    * made changes to remove style elements from container proxy...
    
    * removed body html from v1 of page..
    
    * added README for page comp and bumped up minor version of the package containing form sling models....
    
    * added java doc for the isEdgeDeliveryRequest api...
    
    * added test cases for v2 version of page component....
    
    * added test cases for v2 version of page component....
    
    * made suggested review changes.......
    
    ---------
    
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-5.macromedia.com>
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-5.corp.adobe.com>
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-9.corp.adobe.com>
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-MacBook-Pro-9.local>

---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/form/HtlUtilImpl.java
@@ -0,0 +1,42 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2024 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.internal.form;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.jetbrains.annotations.Nullable;
+
+import com.adobe.cq.forms.core.components.models.form.HtlUtil;
+
+@Model(
+    adaptables = { SlingHttpServletRequest.class, Resource.class },
+    adapters = HtlUtil.class)
+public class HtlUtilImpl implements HtlUtil {
+    @SlingObject(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Nullable
+    private SlingHttpServletRequest request;
+
+    public Boolean isEdgeDeliveryRequest() {
+        if (request != null) {
+            Object isEdgeDelivery = request.getAttribute("com.adobe.aem.wcm.franklin.internal.servlets.FranklinDeliveryServlet");
+            return isEdgeDelivery != null && isEdgeDelivery.equals(Boolean.TRUE);
+        }
+        return false;
+    }
+}
