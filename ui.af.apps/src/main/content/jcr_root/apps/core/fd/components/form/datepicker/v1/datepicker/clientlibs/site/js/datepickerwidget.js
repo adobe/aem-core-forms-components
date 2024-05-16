30119b94f118e0971633c5195db13108c9d91a34
sakarora <sakarora@adobe.com>
2024-05-16 15:48:22 +0530

    Adding the offset for different timezones.

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/datepicker/v1/datepicker/clientlibs/site/js/datepickerwidget.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/datepicker/v1/datepicker/clientlibs/site/js/datepickerwidget.js
@@ -532,9 +532,9 @@ if (typeof window.DatePickerWidget === 'undefined') {
         if (validDate) {
           parsedDate = validDate ? new Date(validDate) : null;
           date = (parsedDate == null || parsedDate == 'Invalid Date') ? date : parsedDate;
-          const timezoneOffset = date.getTimezoneOffset();
-          date.setMinutes(date.getMinutes() + timezoneOffset);
         }
+        const timezoneOffset = date.getTimezoneOffset();
+        date.setMinutes(date.getMinutes() + timezoneOffset);
         this.selectedDay = this.currentDay = date.getDate();
         this.selectedMonth = this.currentMonth = date.getMonth();
         this.selectedYear = this.currentYear = date.getFullYear();
