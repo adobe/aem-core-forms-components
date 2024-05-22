f19c5fb89a33270538aa228b6c81fe15856316c3
Rajat Khurana <rajatkhurana@adobe.com>
2024-05-22 12:32:13 +0530

    FORMS-14275 test cases fixed

--- bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/aemform/AEMFormImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/forms/core/components/internal/models/v1/aemform/AEMFormImplTest.java
@@ -200,6 +200,12 @@ class AEMFormImplTest {
     }
 
     @Test
+    void testGetRoleAttribute() {
+        AEMForm aemform = getAEMFormUnderTest(PATH_FORM_1);
+        assertEquals(null, aemform.getRoleAttribute());
+    }
+
+    @Test
     void testGetBlankThemePath() {
         AEMForm aemform = getAEMFormUnderTest(PATH_FORM_2);
         assertEquals("", aemform.getThemePath());
