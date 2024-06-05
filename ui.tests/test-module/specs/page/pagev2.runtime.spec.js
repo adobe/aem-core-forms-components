4a55a337456b504435b2f92d3ab4c962afccdc12
Talmiz Ahmed <115221634+TalmizAhmed@users.noreply.github.com>
2024-06-05 14:53:37 +0530

    Encode formDefinition with jsonString context (#1242)
    * Encode formDefinition with jsonString context
    
    * Encode formDefinition with jsonString context
    
    * updated the proxy component to point to v2 version of page and wrote a test case to check the json emitted in case of eds servlet originated request
    
    * Encode formDefinition with jsonString context
    
    * updated the proxy component to point to v2 version of page and wrote a test case to check the json emitted in case of eds servlet originated request
    
    * updated the proxy component to point to v2 version of page and wrote a test case to check the json emitted in case of eds servlet originated request
    
    * updated the proxy component to point to v2 version of page and wrote a test case to check the json emitted in case of eds servlet originated request
    
    ---------
    
    Co-authored-by: Deep Prakash Dewanji <deepprakashdewanji@labusers-mbp-9.corp.adobe.com>

--- ui.tests/test-module/specs/page/pagev2.runtime.spec.js
+++ ui.tests/test-module/specs/page/pagev2.runtime.spec.js
@@ -30,3 +30,23 @@ describe("Form with Page component version 2", () => {
         cy.get("html body main form").should("exist");
     })
 })
+
+describe("Form with Page component version 2 for eds rendering", () => {
+    const pagePath = "/bin/franklin.delivery/adobe-rnd/aem-boilerplate-forms/main/content/forms/af/core-components-it/samples/page/afv2.html"
+    beforeEach(() => {
+        cy.openPage(pagePath);
+    });
+
+    it(" form should get rendered with main tag in body and should have pre code tags ", () => {
+        cy.get("html body main div div div pre code").should("exist");
+        cy.get("html body main div div div pre code").then(($codeEle)=>{
+            const encodedJson=$codeEle.get()[0].innerHTML;
+            const formJson=JSON.parse(JSON.parse(encodedJson));
+            expect(formJson).to.have.property("id");
+            expect(formJson).to.have.property("fieldType");
+            expect(formJson).to.have.property(":items");
+            expect(formJson).to.have.property("title");
+
+        })
+    })
+})
