eea40a4e0f76d83a1117887248c9b0b2176733fa
Rajat Khurana <rajatkhurana@adobe.com>
2024-06-26 14:04:27 +0530

    FORMS-14275 test cases added

--- ui.tests/test-module/specs/embedFormsInSiteOverrideConfig/embedFormsInSiteOverrideConfig.spec.js
+++ ui.tests/test-module/specs/embedFormsInSiteOverrideConfig/embedFormsInSiteOverrideConfig.spec.js
@@ -33,22 +33,22 @@ describe("Form in site using embed container", () => {
         expect(formContainer.length).to.equal(2);
       });
 
-      it.skip("should submit on button click and thank you message of embed container must be overridden", () => {
+      it("should submit on button click and thank you message of embed container must be overridden", () => {
             cy.intercept({
             method: 'POST',
             url: '**/adobe/forms/af/submit/*',
         }).as('afSubmission');
-        cy.get(`.cmp-adaptiveform-button__widget`).eq(0).should('be.visible').click().then(() => {
+        cy.get(`.cmp-adaptiveform-button__widget`).eq(0).should('be.visible').wait(500).click().then(() => {
         cy.get('body').should('contain', "Thank You message from sites.\n")
         });
       });
 
-      it.skip("should submit on button click and thank you message of embed container must be overridden", () => {
+      it("should submit on button click and thank you message of embed container must be overridden", () => {
         cy.intercept({
         method: 'POST',
         url: '**/adobe/forms/af/submit/*',
         }).as('afSubmission');
-        cy.get(`.cmp-adaptiveform-button__widget`).eq(1).should('be.visible').click().then(() => {
+        cy.get(`.cmp-adaptiveform-button__widget`).eq(1).should('be.visible').wait(500).click().then(() => {
         cy.get('body').should('contain', "Thank You message from new form in sites.\n")
         });
       });
