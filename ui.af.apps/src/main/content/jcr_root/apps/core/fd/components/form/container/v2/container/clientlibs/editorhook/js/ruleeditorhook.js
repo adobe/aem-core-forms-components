f5f05d6aa82aaf51fa05e9d89a8a3a7af956a1ea
Arun Kumar Attri <aattri@adobe.com>
2024-10-08 12:32:16 +0530

    FORMS-14068: Enable rule-editor spa in core-components

--- ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
+++ ui.af.apps/src/main/content/jcr_root/apps/core/fd/components/form/container/v2/container/clientlibs/editorhook/js/ruleeditorhook.js
@@ -19,35 +19,37 @@
     window.CQ = window.CQ || {};
     window.CQ.FormsCoreComponents = window.CQ.FormsCoreComponents || {};
     window.CQ.FormsCoreComponents.editorhooks =  window.CQ.FormsCoreComponents.editorhooks || {};
-    window.CQ.FormsCoreComponents.editorhooks.v2 = window.CQ.FormsCoreComponents.editorhooks.v2 || {};
     /**
      * Opens the rule editor in a separate iframe for an editable
      * @param editable
      */
-    window.CQ.FormsCoreComponents.editorhooks.openRuleEditor = function (editable) {
-        let existingRuleEditorFrame = document.getElementById('af-rule-editor');
-        if (existingRuleEditorFrame) {
-            existingRuleEditorFrame.parentNode.removeChild(existingRuleEditorFrame);
-        }
-        let ruleEditorFrame = document.createElement('iframe');
-        ruleEditorFrame.setAttribute('id','af-rule-editor');
-        let formContainerPath = getFormContainerPath(editable);
-        if (!formContainerPath) {
-            showAlert();
-        } else {
-            if (typeof window.CQ.FormsCoreComponents.editorhooks.v2.openRuleEditor === 'function') {
-                window.CQ.FormsCoreComponents.editorhooks.v2.openRuleEditor(editable);
+    if (typeof window.CQ.FormsCoreComponents.editorhooks.openRuleEditor !== 'function') {
+        window.CQ.FormsCoreComponents.editorhooks.openRuleEditor = function (editable) {
+            let existingRuleEditorFrame = document.getElementById('af-rule-editor');
+            if (existingRuleEditorFrame) {
+                existingRuleEditorFrame.parentNode.removeChild(existingRuleEditorFrame);
+            }
+            let ruleEditorFrame = document.createElement('iframe');
+            ruleEditorFrame.setAttribute('id','af-rule-editor');
+            let formContainerPath = getFormContainerPath(editable);
+            if (!formContainerPath) {
+                showAlert();
             } else {
-                let ruleEditorUri = '/aem/af/expeditor.html' + getFormContainerPath(editable) + "?fieldPath=" + editable.path + "&fieldId=" + getFieldId(editable);
+                const ruleEditorUri = `/aem/af/expeditor.html${getFormContainerPath(editable)}?fieldPath=${editable.path}&fieldId=${getFieldId(editable)}`;
                 ruleEditorFrame.setAttribute('src', ruleEditorUri);
                 ruleEditorFrame.setAttribute('title', 'AF Rule Editor');
-                ruleEditorFrame.style.display = "block";
-                ruleEditorFrame.style.width = "100%";
-                ruleEditorFrame.style.height = "100%";
-                ruleEditorFrame.style.top = "0";
-                ruleEditorFrame.style.left = "0";
-                ruleEditorFrame.style.position = "fixed";
-                ruleEditorFrame.style.zIndex = "10";
+
+                const styles = {
+                    display: "block",
+                    width: "100%",
+                    height: "100%",
+                    top: "0",
+                    left: "0",
+                    position: "fixed",
+                    zIndex: "10"
+                };
+
+                Object.assign(ruleEditorFrame.style, styles);
                 document.body.appendChild(ruleEditorFrame);
             }
         }
