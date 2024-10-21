bed7db33bb91e96f1827074224ca6c0de34454ed
Arun Kumar Attri <aattri@adobe.com>
2024-10-21 12:34:36 +0530

    FORMS-16943: added dateToDaysSinceEpoch method

--- ui.frontend/src/customFunctions.js
+++ ui.frontend/src/customFunctions.js
@@ -98,5 +98,21 @@ export const customFunctions = {
      * @param {object} globals - An object containing read-only form instance, read-only target field instance and methods for form modifications.
      * @returns {string} - The captcha token.
      */
-    fetchCaptchaToken: cf.fetchCaptchaToken
+    fetchCaptchaToken: cf.fetchCaptchaToken,
+
+    /**
+     * Converts a date to the number of days since the Unix epoch (1970-01-01).
+     *
+     * If the input date is a number, it is assumed to represent the number of days since the epoch,
+     * including both integer and decimal parts. In this case, only the integer part is returned as the number of days.
+     *
+     * @param {string|Date|number} date - The date to convert.
+     * Can be:
+     * - An ISO string (yyyy-mm-dd)
+     * - A Date object
+     * - A number representing the days since the epoch, where the integer part is the number of days and the decimal part is the fraction of the day
+     *
+     * @returns {number} - The number of days since the Unix epoch
+     */
+    dateToDaysSinceEpoch: cf.dateToDaysSinceEpoch
 };
