586b9f28f6fa0f00b903be59d85f69dc429ce3ce
Muskan Gupta <muskgupta@Muskans-MacBook-Pro.local>
2024-11-04 18:09:25 +0530

    Clientlib changes

---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/constants/ThemeConstants.java
@@ -0,0 +1,26 @@
+package com.adobe.cq.forms.core.components.internal.constants;
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~ Copyright 2024 Adobe
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~     http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+public class ThemeConstants {
+
+    public static final String RELATIVE_PATH_METADATA = "/metadata";
+    public static final String THEME_OVERRIDE = "themeOverride";
+    public static final String THEME_REF = "themeRef";
+    public static final String PROPERTY_CLIENTLIB_CATEGORY = "clientlibCategory";
+
+}
