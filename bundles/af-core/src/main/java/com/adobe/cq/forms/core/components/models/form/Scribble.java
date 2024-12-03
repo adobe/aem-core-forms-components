df6cd877b48a3fb0a242d167847d55ae9dbef68f
Rajat Khurana <rajatkhurana@adobe.com>
2024-12-03 14:55:42 +0530

    scribble initial commit

---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/models/form/Scribble.java
@@ -0,0 +1,36 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2024 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+package com.adobe.cq.forms.core.components.models.form;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * Defines the form Scribble Sling Model used for the Scribble component.
+ *
+ * @since com.adobe.cq.forms.core.components.models.form 1.0.0
+ */
+@ConsumerType
+public interface Scribble extends Field {
+
+    /**
+     * Returns the base64 encoded string of the scribble.
+     *
+     * @return Base64 encoded string representing the scribble
+     * @since com.adobe.cq.forms.core.components.models.form 1.0.0
+     */
+    String getValue();
+}
