c37c195251c0982761704c61e4e21857e4b1d2d8
Navneet Agarwal <navneeta@adobe.com>
2024-12-04 12:20:50 +0530

    FORMS-17362 | Fix test for recaptcha authoring

--- ui.tests/test-module/specs/sites/inlineFormInSites.runtime.cy.js
+++ ui.tests/test-module/specs/sites/inlineFormInSites.runtime.cy.js
@@ -30,38 +30,40 @@ describe("Captcha In Sites Runtime Test", () => {
     }
     
     it("submission should pass for enterprise score based captcha",() => {
-        updateEnterpriseConfig(0.5);
-        cy.previewForm(siteWithRecaptchaScore).then((p) => {
-            formContainer = p;
-            expect(formContainer, "formcontainer is initialized").to.not.be.null;
-            cy.get(`div.grecaptcha-badge`).should('exist').then(() => {
-                cy.intercept('POST', /\/adobe\/forms\/af\/submit\/.*/).as('submitForm');
-                const submitForm = () => {
-                    cy.get(`.cmp-adaptiveform-button__widget`).click();
+         if (cy.af.isLatestAddon()) {
+             updateEnterpriseConfig(0.5);
+             cy.previewForm(siteWithRecaptchaScore).then((p) => {
+                 formContainer = p;
+                 expect(formContainer, "formcontainer is initialized").to.not.be.null;
+                 cy.get(`div.grecaptcha-badge`).should('exist').then(() => {
+                     cy.intercept('POST', /\/adobe\/forms\/af\/submit\/.*/).as('submitForm');
+                     const submitForm = () => {
+                         cy.get(`.cmp-adaptiveform-button__widget`).click();
 
-                    // Wait for the submitForm request
-                    return cy.wait('@submitForm',{ timeout: 50000 }).then((interception) => {
-                        if (interception.response.statusCode === 200) {
-                            // Request succeeded
-                            cy.log('Submit request succeeded');
-                            return cy.wrap(true);
-                        } else {
-                            // Request failed
-                            cy.log(`Submit request failed, retrying...`);
-                            return cy.wrap(false);
-                        }
-                    });
-                };
-                // Need to submit multiple times until the form is submitted successfully
-                // Due to below error while validating recaptcha enterprise
-                // https://cloud.google.com/recaptcha-enterprise/docs/faq#returned_browser_error_when_creating_an_assessment_what_should_i_do_about_this
-                cy.waitUntil(() => submitForm(), {
-                    errorMsg: 'Maximum retry limit reached, request did not succeed',
-                    timeout: 50000, // Total timeout (10 seconds)
-                    interval: 5000, // Interval between retries (1 second)
-                });
-            });
-        });
+                         // Wait for the submitForm request
+                         return cy.wait('@submitForm',{ timeout: 50000 }).then((interception) => {
+                             if (interception.response.statusCode === 200) {
+                                 // Request succeeded
+                                 cy.log('Submit request succeeded');
+                                 return cy.wrap(true);
+                             } else {
+                                 // Request failed
+                                 cy.log(`Submit request failed, retrying...`);
+                                 return cy.wrap(false);
+                             }
+                         });
+                     };
+                     // Need to submit multiple times until the form is submitted successfully
+                     // Due to below error while validating recaptcha enterprise
+                     // https://cloud.google.com/recaptcha-enterprise/docs/faq#returned_browser_error_when_creating_an_assessment_what_should_i_do_about_this
+                     cy.waitUntil(() => submitForm(), {
+                         errorMsg: 'Maximum retry limit reached, request did not succeed',
+                         timeout: 50000, // Total timeout (10 seconds)
+                         interval: 5000, // Interval between retries (1 second)
+                     });
+                 });
+             });
+         }
     })
     
 })
