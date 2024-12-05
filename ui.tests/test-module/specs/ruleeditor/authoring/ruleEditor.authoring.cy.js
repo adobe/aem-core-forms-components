923bbf9d7b649566d26e8e1f240aca523cd086ec
Arun Kumar Attri <aattri@adobe.com>
2024-12-05 10:34:32 +0530

    FORMS-17559: fix rule-editor test

--- ui.tests/test-module/specs/ruleeditor/authoring/ruleEditor.authoring.cy.js
+++ ui.tests/test-module/specs/ruleeditor/authoring/ruleEditor.authoring.cy.js
@@ -82,6 +82,7 @@ describe('Rule editor authoring sanity for core-components',function(){
         // Edit rule option not existing on button toolbar
         cy.get(formsSelectors.ruleEditor.action.editRule).should("exist");
         cy.initializeEventHandlerOnChannel("af-rule-editor-initialized").as("isRuleEditorInitialized");
+        cy.wait(1000);
         cy.get(formsSelectors.ruleEditor.action.editRule).click();
 
         // click on  create option from rule editor header
@@ -141,10 +142,12 @@ describe('Rule editor authoring sanity for core-components',function(){
         // Edit rule option not existing on button toolbar
         cy.get(formsSelectors.ruleEditor.action.editRule).should("exist");
         cy.initializeEventHandlerOnChannel("af-rule-editor-initialized").as("isRuleEditorInitialized");
+        cy.wait(1000);
         cy.get(formsSelectors.ruleEditor.action.editRule).click();
 
         // click on  create option from rule editor header
-        cy.get("@isRuleEditorInitialized").its('done').should('equal', true);
+        // cy.get("@isRuleEditorInitialized").its('done').should('equal', true);
+        cy.wait(1000);
         cy.getRuleEditorIframe().find(formsSelectors.ruleEditor.action.createRuleButton).should("be.visible").click();
 
         cy.getRuleEditorIframe().find(formsSelectors.ruleEditor.action.sideToggleButton + ":first").click();
