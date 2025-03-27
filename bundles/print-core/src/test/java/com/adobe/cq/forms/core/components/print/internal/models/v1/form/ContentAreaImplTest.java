e0b0fbc4293380383d46a68e687a786eef6bddb0
dhrsharm <dhrsharm@adobe.com>
2025-03-27 16:05:08 +0530

    feat(print-properties): Moving print related model implementations in separate bundle

---
+++ bundles/print-core/src/test/java/com/adobe/cq/forms/core/components/print/internal/models/v1/form/ContentAreaImplTest.java
@@ -0,0 +1,47 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.print.internal.models.v1.form;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.adobe.cq.forms.core.Utils;
+import com.adobe.cq.forms.core.components.models.form.ContentArea;
+import com.adobe.cq.forms.core.context.FormsCoreComponentTestContext;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+@ExtendWith(AemContextExtension.class)
+public class ContentAreaImplTest {
+
+    private static final String BASE = "/form/contentarea";
+    private static final String CONTENT_ROOT = "/content";
+    private static final String PATH_CONTENTAREA = CONTENT_ROOT + "/contentarea";
+
+    private final AemContext context = FormsCoreComponentTestContext.newAemContext();
+
+    @BeforeEach
+    void setUp() {
+        context.load().json(BASE + FormsCoreComponentTestContext.TEST_CONTENT_JSON, CONTENT_ROOT);
+    }
+
+    @Test
+    void testJSONExport() throws Exception {
+        ContentArea contentarea = Utils.getComponentUnderTest(PATH_CONTENTAREA, ContentArea.class, context);
+        Utils.testJSONExport(contentarea, Utils.getTestExporterJSONPath(BASE, PATH_CONTENTAREA));
+    }
+}
