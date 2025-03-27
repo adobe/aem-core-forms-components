46f0f950b034141340833601f5b8f4f04bb3c716
dhrsharm <dhrsharm@adobe.com>
2025-03-27 16:05:08 +0530

    feat(print-properties): Moving print related model implementations in separate bundle

---
+++ bundles/print-core/src/main/java/com/adobe/cq/forms/core/components/print/internal/models/v1/form/xfa/ItemsImpl.java
@@ -0,0 +1,43 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.print.internal.models.v1.form.xfa;
+
+import java.util.List;
+
+import com.adobe.cq.forms.core.components.models.form.xfa.Items;
+import com.adobe.cq.forms.core.components.models.form.xfa.value.IntegerValue;
+import com.adobe.cq.forms.core.components.print.internal.models.v1.form.xfa.value.IntegerValueImpl;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ItemsImpl implements Items {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<IntegerValueImpl> integers;
+
+    @Override
+    public List<? extends IntegerValue> getIntegers() {
+        return integers;
+    }
+
+    public static Items fromString(String items) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(items, ItemsImpl.class);
+        } catch (JsonProcessingException e) {}
+        return null;
+    }
+}
