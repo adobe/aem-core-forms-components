0cc360a97196fb90bf55e16eed4f631540d276bf
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/print-core/src/main/java/com/adobe/cq/forms/core/components/print/internal/models/v1/form/print/ui/DateTimeEditImpl.java
@@ -0,0 +1,43 @@
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.print.internal.models.v1.form.print.ui;
+
+import com.adobe.cq.forms.core.components.models.form.print.Border;
+import com.adobe.cq.forms.core.components.models.form.print.Margin;
+import com.adobe.cq.forms.core.components.models.form.print.ui.DateTimeEdit;
+import com.adobe.cq.forms.core.components.print.internal.models.v1.form.print.BorderImpl;
+import com.adobe.cq.forms.core.components.print.internal.models.v1.form.print.MarginImpl;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class DateTimeEditImpl implements DateTimeEdit {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private MarginImpl margin;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private BorderImpl border;
+
+    @Override
+    public Margin getMargin() {
+        return margin;
+    }
+
+    @Override
+    public Border getBorder() {
+        return border;
+    }
+}
