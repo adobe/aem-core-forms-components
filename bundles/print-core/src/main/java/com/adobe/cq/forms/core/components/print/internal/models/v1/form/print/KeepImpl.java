1ee739df7154bfea1e71c5bc0f1f0a4b22ca105f
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/print-core/src/main/java/com/adobe/cq/forms/core/components/print/internal/models/v1/form/print/KeepImpl.java
@@ -0,0 +1,57 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.print.internal.models.v1.form.print;
+
+import com.adobe.cq.forms.core.components.models.form.print.Keep;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class KeepImpl implements Keep {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String intact;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String next;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String previous;
+
+    @Override
+    public String getIntact() {
+        return intact;
+    }
+
+    @Override
+    public String getNext() {
+        return next;
+    }
+
+    @Override
+    public String getPrevious() {
+        return previous;
+    }
+
+    public static Keep fromString(String keep) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(keep, KeepImpl.class);
+        } catch (JsonProcessingException e) {}
+        return null;
+    }
+
+}
