5b43cc3b5edac995250d1be804a3135a100123c6
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/models/form/print/Para.java
@@ -0,0 +1,79 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.models.form.print;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public interface Para {
+    @Nullable
+    @JsonProperty("vAlign")
+    default String getVAlign() {
+        return "";
+    }
+
+    @Nullable
+    @JsonProperty("hAlign")
+    default String getHAlign() {
+        return "";
+    }
+
+    @Nullable
+    default String getLineHeight() {
+        return "";
+    }
+
+    @Nullable
+    default String getSpaceAbove() {
+        return "";
+    }
+
+    @Nullable
+    default String getSpaceBelow() {
+        return "";
+    }
+
+    @Nullable
+    default String getMarginLeft() {
+        return "";
+    }
+
+    @Nullable
+    default String getMarginRight() {
+        return "";
+    }
+
+    @Nullable
+    default String getTextIndent() {
+        return "";
+    }
+
+    @Nullable
+    default String getWidows() {
+        return "";
+    }
+
+    @Nullable
+    default String getOrphans() {
+        return "";
+    }
+
+    @Nullable
+    default String getPreserve() {
+        return "";
+    }
+}
