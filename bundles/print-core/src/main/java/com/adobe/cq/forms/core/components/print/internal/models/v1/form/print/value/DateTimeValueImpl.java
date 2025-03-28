46c3c1a6e3303e5a20d22929399309f6a50e908b
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/print-core/src/main/java/com/adobe/cq/forms/core/components/print/internal/models/v1/form/print/value/DateTimeValueImpl.java
@@ -0,0 +1,31 @@
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.print.internal.models.v1.form.print.value;
+
+import com.adobe.cq.forms.core.components.models.form.print.value.DateTimeValue;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class DateTimeValueImpl implements DateTimeValue {
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String content;
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+}
