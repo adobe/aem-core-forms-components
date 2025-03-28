7a2d13e6cea7905b8b575efd73a37ae1937a4389
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/af-core/src/test/java/com/adobe/cq/forms/core/components/models/form/print/VariablesTest.java
@@ -0,0 +1,36 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.models.form.print;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class VariablesTest {
+
+    @Test
+    public void testGetScripts() {
+        // Mock the Variables interface
+        Variables mockVariables = Mockito.mock(Variables.class);
+
+        // Define behavior for the mock
+        Mockito.when(mockVariables.getScripts()).thenCallRealMethod();
+
+        // Assert the behavior
+        assertEquals(null, mockVariables.getScripts());
+    }
+}
