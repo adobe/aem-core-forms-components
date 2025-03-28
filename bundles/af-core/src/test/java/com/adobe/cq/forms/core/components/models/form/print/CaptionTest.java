6f0f7bdf6c5811c7f5ec67c1ac1835b2c5d1c92d
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-03-28 22:09:39 +0530

    feat(print-properties): RTC - Adding UTs for print specific interfaces

---
+++ bundles/af-core/src/test/java/com/adobe/cq/forms/core/components/models/form/print/CaptionTest.java
@@ -0,0 +1,73 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2025 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.forms.core.components.models.form.print;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CaptionTest {
+
+    @Test
+    public void testGetPlacement() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getPlacement()).thenCallRealMethod();
+        assertEquals("", mockCaption.getPlacement());
+    }
+
+    @Test
+    public void testGetReserve() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getReserve()).thenCallRealMethod();
+        assertEquals("", mockCaption.getReserve());
+    }
+
+    @Test
+    public void testGetValue() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getValue()).thenCallRealMethod();
+        assertEquals(null, mockCaption.getValue());
+    }
+
+    @Test
+    public void testGetPara() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getPara()).thenCallRealMethod();
+        assertEquals(null, mockCaption.getPara());
+    }
+
+    @Test
+    public void testGetMargin() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getMargin()).thenCallRealMethod();
+        assertEquals(null, mockCaption.getMargin());
+    }
+
+    @Test
+    public void testGetBorder() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getBorder()).thenCallRealMethod();
+        assertEquals(null, mockCaption.getBorder());
+    }
+
+    @Test
+    public void testGetFont() {
+        Caption mockCaption = Mockito.mock(Caption.class);
+        Mockito.when(mockCaption.getFont()).thenCallRealMethod();
+        assertEquals(null, mockCaption.getFont());
+    }
+}
