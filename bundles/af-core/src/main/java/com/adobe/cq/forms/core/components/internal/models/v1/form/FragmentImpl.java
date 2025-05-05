c96975df6b4e4ff97329194dcb78288ae9261725
DhruvSharma-845 <dhruvsharma845@gmail.com>
2025-05-05 11:29:15 +0530

    feat(print-properties): Adding the change to resolve fragment based on its guideContainer in hierarchy

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/FragmentImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/models/v1/form/FragmentImpl.java
@@ -73,9 +73,9 @@ public class FragmentImpl extends PanelImpl implements Fragment {
     @PostConstruct
     private void initFragmentModel() {
         ResourceResolver resourceResolver = resource.getResourceResolver();
-        if (StringUtils.isNotEmpty(this.channel) && this.channel.equals("print")) {
-            fragmentPath = fragmentPath + "/" + JcrConstants.JCR_CONTENT + "/" + this.channel;
-        }
+
+        fragmentPath = this.getFragmentPathBasedOnChannel(fragmentPath);
+
         fragmentContainer = ComponentUtils.getFragmentContainer(resourceResolver, fragmentPath);
         if (request != null) {
             FormClientLibManager formClientLibManager = request.adaptTo(FormClientLibManager.class);
@@ -84,6 +84,20 @@ public class FragmentImpl extends PanelImpl implements Fragment {
                 formClientLibManager.addClientLibRef(clientLibRef);
             }
         }
+    }
+
+    private String getFragmentPathBasedOnChannel(String fragmentPath) {
+        Resource formContainerResource = resource;
+        String fragmentPathOfChannel = fragmentPath;
+        while (formContainerResource != null) {
+            String resourceType = formContainerResource.getValueMap().get("sling:resourceType", String.class);
+            if (resourceType != null && resourceType.contains("printcontainer")) {
+                fragmentPathOfChannel = fragmentPath + "/" + JcrConstants.JCR_CONTENT + "/" + "print";
+                break;
+            }
+            formContainerResource = formContainerResource.getParent();
+        }
+        return fragmentPathOfChannel;
     }
 
     @JsonIgnore
