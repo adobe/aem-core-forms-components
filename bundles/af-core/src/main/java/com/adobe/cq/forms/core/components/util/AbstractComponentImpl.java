2d3339a2e6b6dc07d73f897ad86359df2d6fd86e
vjystva <vjystva@gmail.com>
2025-05-26 13:59:30 +0530

    Added rule events and passed chanel information to child

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/util/AbstractComponentImpl.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/util/AbstractComponentImpl.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
