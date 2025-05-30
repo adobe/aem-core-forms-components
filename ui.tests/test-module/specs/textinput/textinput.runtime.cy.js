4f5fd25673824bccc57836aecba24776df0e6eaa
sakarora@adobe.com <sakarora@adobe.com>
2025-05-30 19:08:53 +0530

    Fixing Test.

--- ui.tests/test-module/specs/textinput/textinput.runtime.cy.js
+++ ui.tests/test-module/specs/textinput/textinput.runtime.cy.js
@@ -186,7 +186,6 @@ describe("Form Runtime with Text Input", () => {
         const [textbox6, textBox6FieldView] = Object.entries(formContainer._fields)[5];
         const [textbox7, textBox7FieldView] = Object.entries(formContainer._fields)[6];
         const [textbox8, textBox8FieldView] = Object.entries(formContainer._fields)[7];
-debugger;
         const [submitbutton1, fieldView] = Object.entries(formContainer._fields)[12]
 
         // 1. Required
