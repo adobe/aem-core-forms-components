38950fa0a5cded4d7539acbf3528fb3b37d74704
Anshika Gautam <anshikag@adobe.com>
2025-06-11 16:29:07 +0530

    FORMS-20431: Update test cases

--- bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/form/ReservedProperties.java
+++ bundles/af-core/src/main/java/com/adobe/cq/forms/core/components/internal/form/ReservedProperties.java
@@ -163,6 +163,7 @@ public final class ReservedProperties {
     public static final String FD_ENABLE_AUTO_SAVE = "fd:enableAutoSave";
     public static final String FD_AUTO_SAVE_STRATEGY_TYPE = "fd:autoSaveStrategyType";
     public static final String FD_AUTO_SAVE_INTERVAL = "fd:autoSaveInterval";
+    public static final String FD_EXCLUDE_FROM_DOR_IF_HIDDEN = "fd:excludeFromDoRIfHidden";
 
     public static final String FD_XFA_SCRIPTS = "fd:xfaScripts";
 
