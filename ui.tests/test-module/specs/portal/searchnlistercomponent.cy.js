c6610916c6fce6452e86c81cc70ba95c0c73e354
Pankaj <pank@adobe.com>
2025-06-23 23:46:46 +0530

    FORMS-20412 skip some test cases

--- ui.tests/test-module/specs/portal/searchnlistercomponent.cy.js
+++ ui.tests/test-module/specs/portal/searchnlistercomponent.cy.js
@@ -29,7 +29,7 @@
 const sitesSelectors = require('../../libs/commons/sitesSelectors'),
       afConstants = require('../../libs/commons/formsConstants');
 
-describe('Search And Lister - Authoring', function () {
+xdescribe('Search And Lister - Authoring', function () {
     // we can use these values to log in
     const   pagePath = "/content/core-components-examples/library/forms-and-communications-portal/searchlister",
             componentEditPath = pagePath + afConstants.RESPONSIVE_GRID_DEMO_SUFFIX + "/" + afConstants.components.forms.resourceType.fpsnlcomponent.split("/").pop(),
