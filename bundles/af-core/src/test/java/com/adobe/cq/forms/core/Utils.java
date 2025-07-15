2b9b64f4928059bac4cf077a045e26a5654982ac
vjystva <vjystva@gmail.com>
2025-07-15 14:39:40 +0530

    RTC Removed duplicate function

--- bundles/af-core/src/test/java/com/adobe/cq/forms/core/Utils.java
+++ bundles/af-core/src/test/java/com/adobe/cq/forms/core/Utils.java
@@ -128,48 +128,6 @@ public class Utils {
     }
 
     /**
-     * Provided a {@code model} object and an {@code expectedJsonResource} identifying a JSON file in the class path,
-     * this method will test the JSON export of the model and compare it to the JSON object provided by the
-     * {@code expectedJsonResource}.
-     *
-     * @param model
-     *            the Sling Model
-     * @param expectedJsonResource
-     *            the class path resource providing the expected JSON object
-     */
-    public static void testJSONExport(Object model, String expectedJsonResource) {
-        testJSONExport(model, expectedJsonResource, Views.Publish.class);
-    }
-
-    /**
-     * Provided a {@code model} object and an {@code expectedJsonResource} identifying a JSON file in the class path,
-     * this method will test the JSON export of the model and compare it to the JSON object provided by the
-     * {@code expectedJsonResource}.
-     *
-     * @param model
-     *            the Sling Model
-     * @param expectedJsonResource
-     *            the class path resource providing the expected JSON object
-     */
-    public static void testJSONExport(Object model, String expectedJsonResource) {
-        testJSONExport(model, expectedJsonResource, Views.Publish.class);
-    }
-
-    /**
-     * Provided a {@code model} object and an {@code expectedJsonResource} identifying a JSON file in the class path,
-     * this method will test the JSON export of the model and compare it to the JSON object provided by the
-     * {@code expectedJsonResource}.
-     *
-     * @param model
-     *            the Sling Model
-     * @param expectedJsonResource
-     *            the class path resource providing the expected JSON object
-     */
-    public static void testJSONExport(Object model, String expectedJsonResource) {
-        testJSONExport(model, expectedJsonResource, Views.Publish.class);
-    }
-
-    /**
      * The given model is validated against adaptive form specification
      *
      * @param model reference to the sling model
