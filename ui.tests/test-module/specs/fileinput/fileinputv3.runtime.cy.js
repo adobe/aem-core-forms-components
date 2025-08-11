acb368478f5b26c2342bc8ef97ce212d82291649
Anshika Gautam <anshikag@adobe.com>
2025-08-11 20:12:52 +0530

    fileinput duplicate file names fix: * override handleClick method to adapt to v3 onwards structure of fileinput * cypress tests

--- ui.tests/test-module/specs/fileinput/fileinputv3.runtime.cy.js
+++ ui.tests/test-module/specs/fileinput/fileinputv3.runtime.cy.js
@@ -173,6 +173,22 @@ describe("Form with File Input V-3 - Basic Tests", () => {
         getFormObjTest(['empty.pdf', 'empty.pdf', 'empty.pdf', 'empty.pdf', 'empty.pdf'])
     });
 
+    it("check preview functionality of duplicate files", () => {
+        let sampleFileNames = ['sample2.txt', 'sample.txt', 'sample2.txt'];
+        const fileInput = "input[name='fileinput1']";
+        
+        // Attach files
+        cy.attachFile(fileInput, [sampleFileNames[0]]);
+        cy.attachFile(fileInput, [sampleFileNames[1]]);
+        cy.attachFile(fileInput, [sampleFileNames[2]]);
+    
+        checkFilePreviewInFileAttachment(fileInput);
+
+        deleteSelectedFiles(fileInput, sampleFileNames);
+
+        cy.get('.cmp-adaptiveform-fileinput__fileitem').should('have.length', 0);
+    });
+
     it("should toggle description and tooltip", () => {
         cy.toggleDescriptionTooltip(bemBlock, 'fileinput_tooltip_scenario_test');
     })
