1e1095ffbe3aa6ea333e4385e3f9a2048daf91da
Rishi Mehta <rismehta@adobe.com>
2025-09-17 12:40:56 +0530

    Fixing test

--- ui.tests/test-module/specs/telephoneinput/telephoneinput.authoring.cy.js
+++ ui.tests/test-module/specs/telephoneinput/telephoneinput.authoring.cy.js
@@ -92,7 +92,7 @@ describe('Page - Authoring', function () {
     });
   });
 
-  context('Adding removing patterns from design policy', function () {
+  context.only('Adding removing patterns from design policy', function () {
     const templateDataPath = '/conf/core-components-examples/settings/wcm/templates/af-blank-v2/structure',
         telephoneInputPolicy = '[value="' + templateDataPath + '/jcr:content/guideContainer/forms-components-examples/components/form/telephoneinput' + '"] [data-action="POLICY"]',
         authoringPagePath = '/content/forms/af/core-components-it/blank',
@@ -113,7 +113,7 @@ describe('Page - Authoring', function () {
       cy.get(bemDesignDialog).contains('Validation patterns').click();
       // cy.get('[role="tablist"][orientation="horizontal"] [role="tab"]').eq(2).click();
       cy.get('[name="./allowedFormat3"]').eq(0).click();
-      cy.get('[data-granite-coral-multifield-name="./allowedCustomFormats"] button').eq(1).click();
+      cy.get('[data-granite-coral-multifield-name="./allowedCustomFormats"] button').eq(0).click();
       cy.get('[name="./allowedCustomFormats/item0/customFormatKey"]').should('exist').then(() => {
         cy.get('[name="./allowedCustomFormats/item0/customFormatKey"]').focus().type(customKey);
         cy.get('[name="./allowedCustomFormats/item0/customFormatValue"]').focus().type(customValue);
